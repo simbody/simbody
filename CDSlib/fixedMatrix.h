e162421d3da9aa9e6f61c48705807ffda1caa79b
Michael Sherman <sherm1@gmail.com>
2005-09-22 21:07:52 +0000

    fixed capitalization so it will compile on Linux

--- CDSlib/fixedMatrix.h
+++ CDSlib/fixedMatrix.h
@@ -2,7 +2,7 @@
 #ifndef __fixedMatrix_hh__
 #define __fixedMatrix_hh__ 1
 
-#include "FixedMatrixDecl.h"
-#include "FixedMatrixDefs.h"
+#include "fixedMatrixDecl.h"
+#include "fixedMatrixDefs.h"
 
 #endif /* __fixedMatrix_hh__ */
