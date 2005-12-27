922b9a9f439e01462039430075453bcfd75af036
Michael Sherman <sherm1@gmail.com>
2005-12-27 19:25:00 +0000

    reorging

---
+++ MultibodyComputation/IVMSimbodyInterface/include/simbody/IVMSimbodyInterface.h
@@ -0,0 +1,19 @@
+#ifndef IVM_SIMBODY_INTERFACE_H_
+#define IVM_SIMBODY_INTERFACE_H_
+
+/** @file
+ *
+ * This is a Simbody-compatible interface to the IVM-derived rigid body
+ * simulation toolset.
+ */
+
+#include "simbody/SimbodyCommon.h"
+
+#include <iostream>
+
+class IVMSimbodyInterface /* : public RigidBodyMechanicsResource(?) */ {
+};
+
+
+
+#endif /* IVM_SIMBODY_INTERFACE_H_ */
