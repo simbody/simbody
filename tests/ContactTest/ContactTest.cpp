42c7307655632d9eda6f0073e74049a38879f9c5
Michael Sherman <sherm1@gmail.com>
2006-07-08 00:46:21 +0000

    Installed Hunt & Crossley doc as an appendix in the Simbody manual.

--- tests/ContactTest/ContactTest.cpp
+++ tests/ContactTest/ContactTest.cpp
@@ -171,7 +171,7 @@ try {
                               Ground, Transform(firstRubberBallPos+i*Vec3(0,2*rubberBallRadius+1,0)),
                               Mobilizer(Mobilizer::Cartesian, false)));
 
-    const int NHardBalls = 20;
+    const int NHardBalls = 10;
     for (int i=0; i < NHardBalls; ++i)
         balls.push_back(
             bouncers.addRigidBody(hardBallMProps, Transform(),
@@ -196,8 +196,8 @@ try {
     // balls as big as these! In real MKS units, steel's stiffness should
     // be about 220 Gigapascals, i.e., 2.2e11 N/m^2; rubber 0.01GPa=1e7 N/m^2.
     const Real kwall=concrete_planestrain, khard=steel_planestrain, krubber=rubber_planestrain;
-    //const Real cwall=concrete_dissipation, chard=steel_dissipation, crubber=rubber_dissipation;
-    const Real cwall = 0., chard = 0., crubber=0.;
+    const Real cwall=concrete_dissipation, chard=steel_dissipation, crubber=rubber_dissipation;
+    //const Real cwall = 0., chard = 0., crubber=0.;
 
     VTKReporter vtk(mbs, false); // suppress default geometry
     contact.addSphere(pend1, Vec3(0, -linkLength/2, 0), pendBallRadius, krubber, crubber);
