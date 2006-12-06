0373a0ef5c08d022baa6f1638ea907281b7a91b2
Christopher Bruns <christopher.bruns@gmail.com>
2006-12-06 22:44:26 +0000

    Created new simpler test file for oxygen

--- MoleculeModel/tests/TestSimMol0.cpp
+++ MoleculeModel/tests/TestSimMol0.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include "Simbody.h"
-#include "Protein.h"
-#include "Ethane.h"
+#include "OxygenMolecule.h"
 #include "MoleculeModeler.h"
-
-// TODO - include this in Simbody.h
-#include "simbody/internal/NumericalMethods.h"
 
 using namespace std;
 using namespace SimTK;
@@ -14,10 +10,8 @@ using namespace SimTK;
 // modeling layer that has yet to be constructed.
 int main() 
 {
-	Protein protein("ADA");
-	Ethane ethane;
 	MoleculeModeler modeler;
-	modeler.addMolecule(protein).addMolecule(ethane.setPosition(1,2,3));
+	modeler.addMolecule(OxygenMolecule());
 	MultibodySystem system = modeler.system();
 	
 	State state;
@@ -31,11 +25,6 @@ int main()
 		
 		cout << "Energy = " << system.getEnergy(state) << endl;
 		
-		cout 
-			<< "Psi torsion of residue 1 = " 
-			<< modeler.getPsiTorsion(state, *(protein.residue(1))) 
-			<< endl;
-			
 		time += stepInterval;
 	}
 
