9af69d9aa09e03e6fe0f16869fff977e07341e5a
Paul Mitiguy <mitiguy@motiongenesis.com>
2007-10-09 17:37:13 +0000

    Change #includes so this class does not #include Orientation.h -Mitiguy

--- trunk/Mechanics/include/SimTKcommon/internal/Quaternion.h
+++ trunk/Mechanics/include/SimTKcommon/internal/Quaternion.h
@@ -39,7 +39,15 @@
  * -------------------------------------------------------------------------- */
 
 //-----------------------------------------------------------------------------
-#include "SimTKcommon/internal/Orientation.h"
+#include "SimTKcommon/internal/common.h"
+#include "SimTKcommon/Constants.h"
+#include "SimTKcommon/internal/Scalar.h"
+#include "SimTKcommon/internal/SmallMatrix.h"
+#include "SimTKcommon/internal/UnitVec.h"
+//-----------------------------------------------------------------------------
+#include <iosfwd>  // Forward declaration of iostream
+//-----------------------------------------------------------------------------
+
 
 //-----------------------------------------------------------------------------
 namespace SimTK {
