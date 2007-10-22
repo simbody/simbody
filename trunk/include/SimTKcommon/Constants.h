b829ac535f3fecd91562aaf5749cc5408978a14c
Michael Sherman <sherm1@gmail.com>
2007-10-22 00:28:20 +0000

    Added doxygen-compatible comments for all the #defines of constants.

--- trunk/include/SimTKcommon/Constants.h
+++ trunk/include/SimTKcommon/Constants.h
@@ -59,9 +59,8 @@
  *
  * This file is self-contained and can be included in ANSI C programs
  * as well as C++.
- */
-
-/*
+ *
+ * <pre>
  * Unit systems
  *
  *            SI (MKS)           MD                     KCAL-ANGSTROM
@@ -80,6 +79,8 @@
  * force      N (kg-m/s^2)    kJ/(mol-nm) = TN/mol      kcal/(mol-Å)
  *                              (Da-nm/ps^2) (T=10^12)    (418.4 Da-Å/ps^2)
  *
+ * </pre>
+ *
  * We always keep angles in radians internally, which are unitless. However,
  * most humans prefer degrees where 1 degree = Pi/180 radians so we provide
  * convenient conversions.
@@ -89,7 +90,8 @@
     /* MATHEMATICAL CONSTANTS */
     /**************************/
 
-/*
+/** @name Mathematical Constants
+ *
  * These are some common unitless numerical constants evaluated to 64 digits and
  * written here in maximal (long double) precision. (These values were generated using
  * the symbolic calculator Maple which is part of Matlab's Symbolic 
@@ -105,37 +107,84 @@
  * which can only be generated for specific types.
  */
 
+/*@{    Mathematical Constants */
+
+/** The ratio pi of a circle's circumference to its diameter in Euclidean geometry.
+ * @par uncertainty
+ *      approximation of an exact value
+ */
 #define SimTK_PI     3.141592653589793238462643383279502884197169399375105820974944592L
+
+/** e, or exp(1).
+ * @par uncertainty
+ *      approximation of an exact value
+ */
 #define SimTK_E      2.718281828459045235360287471352662497757247093699959574966967628L
 
-/* log2(e), log10(e) */
-#define SimTK_LOG2E  1.442695040888963407359924681001892137426645954152985934135449407L
-#define SimTK_LOG10E 4.342944819032518276511289189166050822943970058036665661144537832e-1L
-
-/* sqrt(2), sqrt(3), cubeRoot(2), cubeRoot(3) */
-#define SimTK_SQRT2  1.414213562373095048801688724209698078569671875376948073176679738L
-#define SimTK_SQRT3  1.732050807568877293527446341505872366942805253810380628055806979L
-#define SimTK_CBRT2  1.259921049894873164767210607278228350570251464701507980081975112L
-#define SimTK_CBRT3  1.442249570307408382321638310780109588391869253499350577546416195L
-
-/* ln(2), ln(10) */
+/** The natural (base e) logarithm of 2.
+ * @par uncertainty
+ *      approximation of an exact value
+ * @see SimTK_E
+ */
 #define SimTK_LN2    6.931471805599453094172321214581765680755001343602552541206800095e-1L
+
+/** The natural (base e) logarithm of 10.
+ * @par uncertainty
+ *      approximation of an exact value
+ * @see SimTK_E
+ */
 #define SimTK_LN10   2.302585092994045684017991454684364207601101488628772976033327901L
 
+/** log2(e).
+ * @par uncertainty
+ *      approximation of an exact value
+ */
+#define SimTK_LOG2E  1.442695040888963407359924681001892137426645954152985934135449407L
+
+/** log10(e).
+ * @par uncertainty
+ *      approximation of an exact value
+ */
+#define SimTK_LOG10E 4.342944819032518276511289189166050822943970058036665661144537832e-1L
+
+/** The square root of 2.
+ * @par uncertainty
+ *      approximation of an exact value
+ */
+#define SimTK_SQRT2  1.414213562373095048801688724209698078569671875376948073176679738L
+
+/** The square root of 3.
+ * @par uncertainty
+ *      approximation of an exact value
+ */
+#define SimTK_SQRT3  1.732050807568877293527446341505872366942805253810380628055806979L
+
+/** The cube root of 2.
+ * @par uncertainty
+ *      approximation of an exact value
+ */
+#define SimTK_CBRT2  1.259921049894873164767210607278228350570251464701507980081975112L
+
+/** The cube root of 3.
+ * @par uncertainty
+ *      approximation of an exact value
+ */
+#define SimTK_CBRT3  1.442249570307408382321638310780109588391869253499350577546416195L
+
+/*@}    end of Mathematical Constants */
 
     /**********************/
     /* PHYSICAL CONSTANTS */
     /**********************/
 
-/*
- * Provenance
- * ----------
+/** @name Physical Constants
+ *
+ * @par Provenance
  * These constants are from the CODATA 2002 set from the NIST Physics Laboratory web site
- * physics.nist.gov/constants. (NIST SP 961 Dec,2005)
+ * http://physics.nist.gov/constants. (NIST SP 961 Dec,2005)
  * Ref: P.J. Mohr and B.N. Taylor, Rev. Mod. Phys. 77(1) (2005).
  * 
- * Uncertainty
- * -----------
+ * @par Uncertainty
  * Uncertainties are given in the CODATA set as the one-std-deviation uncertainty in the
  * last 2 digits of the given value. That means that there is about a 68% chance that
  * the last two digits are as shown +/- the uncertainty.
@@ -144,130 +193,323 @@
  * http://physics.nist.gov/cuu/Uncertainty/combination.html):
  * Assume measured quantities are x1, y1 with u1=uncertainty(x1), u2=uncertainty(x2).
  * We want to combine them into a new quantity y and calculate u=uncertainty(y).
+ * <pre>
  * Addition rule: y    = a1*x1 + a2*x2 for factors a1,a2.
  *  then          u^2  = a1*u1^2 + a2*u2^2
  * Multiplication rule y = a*x1^e1*x2^e2 for factor a and exponents e1,e2.
  * Let ur1=u1/|x1|, ur2=u2/|x2| be the relative uncertainties, ur is u/|y|.
  *  then          ur^2 = e1^2*ur1^2 + e2^2*ur2^2, u = ur*|y|
+ * </pre>
  */
 
-/* Avogadro's number (NA) is defined as the number of atoms in 12g of pure Carbon-12 in
+/*@{    Physical Constants */
+
+/** 
+ * Avogadro's number (NA) is defined as the number of atoms in 12g of pure Carbon-12 in
  * its unbound, rest state. The number is 1 mole (mol).
+ * @par uncertainty
+ *      10e16
  */
-#define SimTK_AVOGADROS_NUMBER              6.0221415e23L       /* uncertainty: 10e16 */
+#define SimTK_AVOGADROS_NUMBER 6.0221415e23L
 
-/*
+/**
+ * Mass of a proton in MD units.
+ *
  * The atomic mass unit u (or amu) is defined as 1/12 of the mass of a Carbon-12 atom,
  * unbound and in its rest state. This definition matched to Avogadro's number's definition
  * ensures that 1 mole of particles of mass 1u each has total mass exactly 1g. This is
  * synonymous with the dalton (Da), with units of g/mole, so 1u = 1Dalton = 1g/mole.
  * We will use Da for this mass unit, with kDa being a common mass measure for 
  * large biomolecules.
+ *
+ * @par uncertainty
+ *      13e-11
+ * @see SimTK_AVOGADROS_NUMBER
  */
-#define SimTK_MASS_OF_ELECTRON_IN_MD        5.4857990945e-4L    /* uncertainty: 24e-14 */
-#define SimTK_MASS_OF_PROTON_IN_MD          1.00727646688L      /* uncertainty: 13e-11 */
-#define SimTK_MASS_OF_NEUTRON_IN_MD         1.00866491560L      /* uncertainty: 55e-11 */
+#define SimTK_MASS_OF_PROTON_IN_MD 1.00727646688L
 
-/* Atomic charge unit e expressed in MKS unit of Coulombs */
-#define SimTK_CHARGE_OF_PROTON_IN_SI        1.60217653e-19L     /* uncertainty: 14e-27 */
-#define SimTK_CHARGE_OF_PROTON_IN_MD        1.L                 /* exact (duh!) */
+/**
+ * Mass of a neutron in MD units.
+ * @par uncertainty
+ *      55e-11
+ * @see SimTK_MASS_OF_PROTON_IN_MD
+ */
+#define SimTK_MASS_OF_NEUTRON_IN_MD 1.00866491560L
 
-/* This is the charge of 1 mole of protons, expressed in Coulombs
+/**
+ * Mass of an electron in MD units.
+ * @par uncertainty
+ *      24e-14
+ * @see SimTK_MASS_OF_PROTON_IN_MD
+ */
+#define SimTK_MASS_OF_ELECTRON_IN_MD 5.4857990945e-4L
+
+/**
+ * Atomic charge unit e expressed in MKS unit of Coulombs.
+ * The charge on an electron is just the negative of this value.
+ * @par uncertainty
+ *      14e-27
+ */
+#define SimTK_CHARGE_OF_PROTON_IN_SI 1.60217653e-19L
+
+/**
+ * Atomic charge unit e expressed in MD units, which uses e as its charge unit!
+ * The charge on an electron is just the negative of this value.
+ * @par uncertainty
+ *      exact (duh!)
+ */
+#define SimTK_CHARGE_OF_PROTON_IN_MD 1.L
+
+/**
+ * The charge of 1 mole of protons, expressed in Coulombs.
+ * <pre>
  *    1.60217653(14)e-19 C/e * 6.0221415(10)e23 = 9.6485338(18)e+4
+ * </pre>
+ * @par uncertainty
+ *      18e-3 
  */
-#define SimTK_MOLAR_CHARGE_IN_SI            9.6485338e+4L       /* uncertainty: 18e-3 */
-#define SimTK_MOLAR_CHARGE_IN_MD            SimTK_AVOGADROS_NUMBER
+#define SimTK_MOLAR_CHARGE_IN_SI 9.6485338e+4L
 
-/* Speed of light c is exact in MKS units (m/s), or in MD (nm/ps) */
-#define SimTK_LIGHTSPEED_IN_SI              2.99792458e+8L      /* exact */
-#define SimTK_LIGHTSPEED_IN_MD              2.99792458e+5L      /* exact */
-
-/* The force between two point masses m1,m2 separated by a distance d is
- *       F = -G m1*m2/d^2  ("-" indicating an attractive force)
- * Newton's gravitational constant G in  N-m^2/kg^2 = m^3 kg^-1 s^-2
+/**
+ * The charge of 1 mole of protons, expressed in MD units where the unit
+ * of charge is just the charge on one proton. So in MD units this is just
+ * Avogadro's number.
+ * @see SimTK_AVOGADROS_NUMBER
  */
-#define SimTK_GRAVITATIONAL_CONSTANT_IN_SI  6.6742e-11L         /* uncertainty: 10e-15 */    
+#define SimTK_MOLAR_CHARGE_IN_MD SimTK_AVOGADROS_NUMBER
 
+/**
+ * Speed of light c is exact in MKS units of m/s.
+ * @par uncertainty
+ *      exact
+ * @see SimTK_LIGHTSPEED_IN_MD
+ */
+#define SimTK_LIGHTSPEED_IN_SI 2.99792458e+8L
 
-/* Newton's gravitational constant G in  (kJ/mol)-nm^2/u^2 = nm^3 u^-1 ps^-2
+/**
+ * Speed of light c is exact in MD units of nm/ps.
+ * @par uncertainty
+ *      exact
+ * @see SimTK_LIGHTSPEED_IN_SI
+ */
+#define SimTK_LIGHTSPEED_IN_MD 2.99792458e+5L
+
+/** 
+ * Newton's gravitational constant G in N-m^2/kg^2 = m^3 kg^-1 s^-2.
+ * The force between two point masses m1,m2 separated by a distance d is
+ *     <pre> F = -G m1*m2/d^2 </pre> 
+ * (with the "-" indicating an attractive force).
+ * @par uncertainty
+ *      10e-15
+ * @see SimTK_GRAVITATIONAL_CONSTANT_IN_MD
+ */
+#define SimTK_GRAVITATIONAL_CONSTANT_IN_SI 6.6742e-11L   
+
+/**
+ * Newton's gravitational constant G in (kJ/mol)-nm^2/u^2 = nm^3 u^-1 ps^-2.
+ * <pre>
  * Conversion is (nm/m)^3 (u/kg)^-1 (ps/s)^-2
  *          = 1.66053886(28)e-24L     (uncertainty: 28e-32)
+ * </pre>
  * This is why gravity doesn't matter in molecular systems. Don't try
  * this in single precision -- you'll run out of exponent!
+ * @par uncertainty
+ *      17e-39
+ * @see SimTK_GRAVITATIONAL_CONSTANT_IN_SI
  */
-#define SimTK_GRAVITATIONAL_CONSTANT_IN_MD  1.10827e-34L        /* uncertainty: 17e-39 */
+#define SimTK_GRAVITATIONAL_CONSTANT_IN_MD 1.10827e-34L
 
-/* Free space magnetic permeability constant mu0
+/**
+ * Free space magnetic permeability constant mu0 in SI units (exact).
+ * <pre>
  *   = 4*pi * 1e-7 exactly in N/A^2 (Newtons/Ampere^2) = kg-m/C^2
+ * </pre>
+ * @par uncertainty
+ *      approximation of an exact quantity
+ * @see SimTK_ELECTRIC_PERMITTIVITY_IN_SI
  */
 #define SimTK_MAGNETIC_PERMEABILITY_IN_SI  \
-    1.256637061435917295385057353311801153678867759750042328389977837e-6L   /* approx of exact */
+    1.256637061435917295385057353311801153678867759750042328389977837e-6L
 
-/* Convert kg->g/mole, m->nm, C->e = (4*pi*1e5)*1.60217653e-19^2*6.0221415e23 (not exact) */
-#define SimTK_MAGNETIC_PERMEABILITY_IN_MD   1.94259179e-8L      /* uncertainty: 47e-16 */
+/**
+ * Free space magnetic permeability constant mu0 in MD units (not exact).
+ * <pre>
+ * Convert kg->g/mole, m->nm, C->e = (4*pi*1e5)*1.60217653e-19^2*6.0221415e23
+ *      (exact in SI units, but not exact here)
+ * </pre>
+ * @par uncertainty
+ *      47e-16
+ * @see SimTK_ELECTRIC_PERMITTIVITY_IN_MD
+ */
+#define SimTK_MAGNETIC_PERMEABILITY_IN_MD   1.94259179e-8L
 
-/* Free space permittivity constant e0 = 1/(mu0*c^2) Farad/m = Coulomb^2/(N-m^2) (exact in SI units) */
+/**
+ * Free space permittivity constant e0 = 1/(mu0*c^2) Farad/m = Coulomb^2/(N-m^2) (exact in SI units).
+ * @par uncertainty
+ *      approximation of an exact quantity
+ * @see SimTK_MAGNETIC_PERMEABILITY_IN_SI
+ */
 #define SimTK_ELECTRIC_PERMITTIVITY_IN_SI  \
     8.854187817620389850536563031710750260608370166599449808102417149e-12L  /* approx of exact */
 
-/* Use MD permeability and MD lightspeed: e0=1/(mu0*c^2) e^2/(kN-nm^2) */
-#define SimTK_ELECTRIC_PERMITTIVITY_IN_MD   5.7276575e-4L       /* uncertainty: 14e-11 */
-
-/* Coulomb's constant kappa = 1/(4pi*e0)=1e-7*c^2 N-m^2/Coulomb^2 (exact in SI units).
- * This is the constant that appears in Coulomb's law f(r)= kappa*q1*q2/r^2
+/**
+ * Free space permittivity constant e0=1/(mu0*c^2) e^2/(kN-nm^2) using MD permeability and
+ * MD lightspeed.
+ * @par uncertainty
+ *      14e-11
+ * @see SimTK_MAGNETIC_PERMEABILITY_IN_MD
  */
-#define SimTK_COULOMB_CONSTANT_IN_SI       8.9875517873681764e+9L   /* exact */
+#define SimTK_ELECTRIC_PERMITTIVITY_IN_MD 5.7276575e-4L
 
-/* Coulomb's consant in MD units uses MD e0 & c: 
- * kappa = 1/(4*pi*e0)=1e5*1.60217653e-19^2*6.0221415e23*c^2 kN-nm^2/e^2 (=kJ-nm/e^2) (not exact)
+/**
+ * Coulomb's constant kappa = 1/(4pi*e0)=1e-7*c^2 N-m^2/Coulomb^2 (exact in SI units).
+ * This is the constant that appears in Coulomb's law f(r)= kappa*q1*q2/r^2.
+ * @par uncertainty
+ *      exact
  */
-#define SimTK_COULOMB_CONSTANT_IN_MD       1.38935456e+2L       /* uncertainty: 33e-6 */
+#define SimTK_COULOMB_CONSTANT_IN_SI 8.9875517873681764e+9L
 
-/* Here is the same thing in kcal-Angstroms/e^2 (exact conversion from MD units) */
-#define SimTK_COULOMB_CONSTANT_IN_KCAL_ANGSTROM 3.32063711e+2L  /* uncertainty: 80e-6 */
+/**
+ * Coulomb's constant kappa = 1/(4*pi*e0) in MD units.
+ * This is the constant that appears in Coulomb's law f(r)= kappa*q1*q2/r^2.
+ * <pre>
+ * Coulomb's consant in MD units uses MD e0 & c: 
+ *   1/(4*pi*e0)=1e5*1.60217653e-19^2*6.0221415e23*c^2 kN-nm^2/e^2 (=kJ-nm/e^2)
+ *     (exact in SI units but not exact in MD)
+ * </pre>
+ * @par uncertainty
+ *      33e-6
+ */
+#define SimTK_COULOMB_CONSTANT_IN_MD 1.38935456e+2L
 
-/* This is the gas constant R in (J/mol)/K. */
-#define SimTK_MOLAR_GAS_CONSTANT_SI  8.314472L                  /* uncertainty: 15e-6 */
+/**
+ * Coulomb's constant kappa = 1/(4*pi*e0) in kcal-Angstroms/e^2.
+ * This is the constant that appears in Coulomb's law f(r)= kappa*q1*q2/r^2.
+ * This is an exact conversion from MD units (which are inexact).
+ * @par uncertainty
+ *      80e-6
+ */
+#define SimTK_COULOMB_CONSTANT_IN_KCAL_ANGSTROM 3.32063711e+2L
 
-/* In this case MD and MKS differ only by kJ vs J: (kJ/mol)/K here. */
-#define SimTK_MOLAR_GAS_CONSTANT_MD  8.314472e-3L               /* uncertainty: 15e-9 */
+/**
+ * This is the gas constant R in (J/mol)/K. 
+ * @par uncertainty
+ *      15e-6
+ */
+#define SimTK_MOLAR_GAS_CONSTANT_SI 8.314472L
 
-/* Just need to convert kJ to kcal here */
-#define SimTK_MOLAR_GAS_CONSTANT_KCAL_ANGSTROM 1.9872065e-3L    /* uncertainty: 36e-10 */
+/**
+ * This is the gas constant R in (kJ/mol)/K. 
+ * This is an exact conversion from SI units, differing only in the use of kJ 
+ * here vs. J in SI.
+ * @par uncertainty
+ *      15e-9
+ */
+#define SimTK_MOLAR_GAS_CONSTANT_MD 8.314472e-3L
 
-/* Units are joules/kelvin in SI; i.e. divide R by NA */
-#define SimTK_BOLTZMANN_CONSTANT_SI  1.3806505e-23L             /* uncertainty: 24e-30 */
+/**
+ * This is the gas constant R in (kcal/mol)/K. 
+ * This is an exact conversion from MD units, differing only in the use of kcal 
+ * here vs. kJ in MD.
+ * @par uncertainty
+ *      36e-10
+ */
+#define SimTK_MOLAR_GAS_CONSTANT_KCAL_ANGSTROM 1.9872065e-3L
 
-/* Units are (kJ/mol)/kelvin; same as R */
+/**
+ * Boltzmann's constant in SI units of joules/kelvin; just divide R by NA.
+ * @par uncertainty
+ *      24e-30
+ */
+#define SimTK_BOLTZMANN_CONSTANT_SI  1.3806505e-23L
+
+/**
+ * Boltzmann's constant in MD units of (kJ/mol)/kelvin; same as R.
+ * @see SimTK_MOLAR_GAS_CONSTANT_MD
+ */
 #define SimTK_BOLTZMANN_CONSTANT_MD  SimTK_MOLAR_GAS_CONSTANT_MD
 
-/* Units are (kcal/mol)/kelvin; same as R */
+/**
+ * Boltzmann's constant in Kcal-Angstrom units of (kcal/mol)/kelvin; same as R.
+ * @see SimTK_MOLAR_GAS_CONSTANT_KCAL_ANGSTROM
+ */
 #define SimTK_BOLTZMANN_CONSTANT_KCAL_ANGSTROM SimTK_MOLAR_GAS_CONSTANT_KCAL_ANGSTROM
 
+/*@}    end of Physical Constants */
 
     /***************************/
     /* UNIT CONVERSION FACTORS */
     /***************************/
 
-/* In each case you should *multiply* by the given constant to perform the 
- * indicated conversion; divide by the constant to perform the inverse conversion.
+/** @name Unit Conversion Factors
+ *
+ * In each case here, given a value in the units mentioned first in the name,
+ * you should multiply by the given constant to produce the equivalent
+ * quantity measured in the units that appear second in the name. You can 
+ * perform the reverse conversion by dividing by the constant, or by using
+ * another conversion constant with the names reversed if one is supplied here.
  */
 
+/*@{    Unit Conversion Factors*/
+
+/** 
+ * Convert radians to degrees.
+ * @par uncertainty
+ *       approximation of an exact quantity
+ * @see SimTK_DEGREE_TO_RADIAN
+ */
 #define SimTK_RADIAN_TO_DEGREE 5.729577951308232087679815481410517033240547246656432154916024386e+1L
+
+/** 
+ * Convert degrees to radians.
+ * @par uncertainty
+ *       approximation of an exact quantity
+ * @see SimTK_RADIAN_TO_DEGREE
+ */
 #define SimTK_DEGREE_TO_RADIAN 1.745329251994329576923690768488612713442871888541725456097191440e-2L
 
-/* Kcal <-> Kjoule conversion is exact (4.184 and 1/4.184)
- * Of course these work for kcal/mol <-> kj/mol too.
+/** 
+ * Convert Kcal to Kjoule (also Kcal/mol to Kjoule/mol).
+ * @par uncertainty
+ *         exact
+ * @see SimTK_KJOULE_TO_KCAL
  */
-#define SimTK_KCAL_TO_KJOULE     4.184L /* exact */
-#define SimTK_KJOULE_TO_KCAL     2.390057361376673040152963671128107074569789674952198852772466539e-1L
+#define SimTK_KCAL_TO_KJOULE 4.184L /* exact */
 
-/* Atomic mass unit (a.k.a. Dalton) to g. This is 1/NA (NA=avogadro's number); see below. */
-#define SimTK_DALTON_TO_GRAM     1.66053886e-24L    /* uncertainty: 28e-32 */
+/** 
+ * Convert Kjoule to Kcal (also Kjoule/mol to Kcal/mol).
+ * @par uncertainty
+ *         approximation of an exact quantity
+ * @see SimTK_KCAL_TO_KJOULE
+ */
+#define SimTK_KJOULE_TO_KCAL 2.390057361376673040152963671128107074569789674952198852772466539e-1L
 
-/* Proton charge unit to Coulomb = electron volt to Joule */
+/** 
+ * Convert atomic mass unit (amu, Dalton) to g. This is 1/NA (NA=avogadro's number).
+ * @par uncertainty
+ *         28e-32
+ * @see SimTK_AVOGADROS_NUMBER
+ */
+#define SimTK_DALTON_TO_GRAM     1.66053886e-24L
+
+/** 
+ * Convert proton charge units to Coulombs. This is the same as the
+ * conversion from electron volts to Joules, and both are just the
+ * charge of a proton in SI units.
+ * @see SimTK_CHARGE_OF_PROTON_IN_SI
+ * @see SimTK_EV_TO_JOULE
+ */
 #define SimTK_E_TO_COULOMB       SimTK_CHARGE_OF_PROTON_IN_SI
+
+/** 
+ * Convert electron volts to Joules. This is the same as the
+ * conversion from proton charge units to Coulombs, and both are just the
+ * charge of a proton in SI units.
+ * @see SimTK_CHARGE_OF_PROTON_IN_SI
+ * @see SimTK_E_TO_COULOMB
+ */
 #define SimTK_EV_TO_JOULE        SimTK_CHARGE_OF_PROTON_IN_SI
 
+/*@}    end of Unit Conversion Factors */
     
 #endif /* SimTK_SimTKCOMMON_CONSTANTS_H_ */
