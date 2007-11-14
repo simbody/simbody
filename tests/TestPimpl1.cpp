e98ff9a68d736e32a67b81492aa3fa83bf99c6d3
Christopher Bruns <christopher.bruns@gmail.com>
2007-11-14 00:51:34 +0000

    Checked in test program that exercises recent PIMPL troubles

---
+++ tests/TestPimpl1.cpp
@@ -0,0 +1,11 @@
+#include "SimTKsimbody.h"
+#include "SimTKcommon/internal/PrivateImplementation_Defs.h"
+
+#include <iostream>
+
+int main() 
+{
+    std::cout << "passed" << std::endl;
+
+    return 0;
+}
