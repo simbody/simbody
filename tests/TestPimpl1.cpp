41772f5705919869ce7b7d8ec074991e47008144
Michael Sherman <sherm1@gmail.com>
2007-11-16 00:42:38 +0000

    Trying to deal with extern template instantiation for MobilizedBody.

--- tests/TestPimpl1.cpp
+++ tests/TestPimpl1.cpp
@@ -1,5 +1,18 @@
 
 #include "SimTKsimbody.h"
+#pragma warning(disable:4661)
+class Element;
+class ElementRep;
+
+//extern template class /*__declspec(dllimport)*/ SimTK::PIMPLHandle<Element, ElementRep>;
+
+
+class /*__declspec(dllexport)*/ Element : public SimTK::PIMPLHandle<Element,ElementRep> {
+public:
+};
+
+
+
 #include "SimTKcommon/internal/PrivateImplementation_Defs.h"
 
 #include <iostream>
@@ -8,5 +21,15 @@ int main()
 {
     std::cout << "passed" << std::endl;
 
+    Element e;
+
     return 0;
 }
+
+
+class ElementRep : public SimTK::PIMPLImplementation<Element,ElementRep> {
+public:
+    ElementRep* clone() const {return new ElementRep();}
+};
+
+template class SimTK::PIMPLHandle<Element,ElementRep>;
