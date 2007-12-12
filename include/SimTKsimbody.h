1bed6d4023b1c2d9b70f270523fce4a71af5cf56
Peter Eastman <peter.eastman@gmail.com>
2007-12-12 01:21:06 +0000

    Created LocalEnergyMinimizer

--- include/SimTKsimbody.h
+++ include/SimTKsimbody.h
@@ -57,5 +57,6 @@
 #include "simbody/internal/TextDataEventReporter.h"
 #include "simbody/internal/ObservedPointFitter.h"
 #include "simbody/internal/VelocityRescalingThermostat.h"
+#include "simbody/internal/LocalEnergyMinimizer.h"
 
 #endif // SimTK_SIMBODY_SimTKSIMBODY_H_
