bd7ce6b70999d67fd9cca6735a6c059bf2ddd959
Michael Sherman <sherm1@gmail.com>
2008-02-28 16:40:17 +0000

    Updated to use the new MobilizedBody method names.

--- tests/TestMobilizedBody.cpp
+++ tests/TestMobilizedBody.cpp
@@ -79,31 +79,31 @@ void testCalculationMethods() {
     // Test the low level methods for transforming points and vectors.
     
     const Vec3 point(0.5, 1, -1.5);
-    assertEqual(b1.locateBodyPointOnGround(state, Vec3(0)), b1.getBodyOriginLocation(state));
-    assertEqual(b1.locateGroundPointOnBody(state, b1.locateBodyPointOnGround(state, point)), point);
-    assertEqual(b2.locateGroundPointOnBody(state, b1.locateBodyPointOnGround(state, point)), b1.locateBodyPointOnBody(state, point, b2));
-    assertEqual(b2.locateGroundPointOnBody(state, b1.locateBodyPointOnGround(state, Vec3(0))).norm(), (b1.getBodyOriginLocation(state)-b2.getBodyOriginLocation(state)).norm());
-    assertEqual(b2.locateBodyMassCenterOnGround(state), b2.locateBodyPointOnGround(state, b2.getBodyMassCenterStation(state)));
-    assertEqual(b1.expressBodyVectorInGround(state, Vec3(0)), Vec3(0));
-    assertEqual(b1.expressBodyVectorInGround(state, point), b1.getBodyRotation(state)*point);
-    assertEqual(b1.expressGroundVectorInBody(state, b1.expressBodyVectorInGround(state, point)), point);
-    assertEqual(b2.expressGroundVectorInBody(state, b1.expressBodyVectorInGround(state, point)), b1.expressBodyVectorInBody(state, point, b2));
+    assertEqual(b1.findStationLocationInGround(state, Vec3(0)), b1.getBodyOriginLocation(state));
+    assertEqual(b1.findStationAtGroundPoint(state, b1.findStationLocationInGround(state, point)), point);
+    assertEqual(b2.findStationAtGroundPoint(state, b1.findStationLocationInGround(state, point)), b1.findStationLocationInAnotherBody(state, point, b2));
+    assertEqual(b2.findStationAtGroundPoint(state, b1.findStationLocationInGround(state, Vec3(0))).norm(), (b1.getBodyOriginLocation(state)-b2.getBodyOriginLocation(state)).norm());
+    assertEqual(b2.findMassCenterLocationInGround(state), b2.findStationLocationInGround(state, b2.getBodyMassCenterStation(state)));
+    assertEqual(b1.expressVectorInGroundFrame(state, Vec3(0)), Vec3(0));
+    assertEqual(b1.expressVectorInGroundFrame(state, point), b1.getBodyRotation(state)*point);
+    assertEqual(b1.expressGroundVectorInBodyFrame(state, b1.expressVectorInGroundFrame(state, point)), point);
+    assertEqual(b2.expressGroundVectorInBodyFrame(state, b1.expressVectorInGroundFrame(state, point)), b1.expressVectorInAnotherBodyFrame(state, point, b2));
     
     // Test the routines for mapping locations, velocities, and accelerations.
     
     Vec3 r, v, a;
-    b1.calcBodyFixedPointLocationVelocityAndAccelerationInGround(state, point, r, v, a);
-    assertEqual(v, b1.calcBodyFixedPointVelocityInGround(state, point));
-    assertEqual(a, b1.calcBodyFixedPointAccelerationInGround(state, point));
+    b1.findStationLocationVelocityAndAccelerationInGround(state, point, r, v, a);
+    assertEqual(v, b1.findStationVelocityInGround(state, point));
+    assertEqual(a, b1.findStationAccelerationInGround(state, point));
     {
         Vec3 r2, v2;
-        b1.calcBodyFixedPointLocationAndVelocityInGround(state, point, r2, v2);
+        b1.findStationLocationAndVelocityInGround(state, point, r2, v2);
         assertEqual(r, r2);
         assertEqual(v, v2);
     }
-    assertEqual(b1.calcBodyFixedPointVelocityInGround(state, Vec3(0)), b1.getBodyOriginVelocity(state));
-    assertEqual(b1.calcBodyFixedPointAccelerationInGround(state, Vec3(0)), b1.getBodyOriginAcceleration(state));
-    assertEqual(b1.calcBodyFixedPointVelocityInGround(state, point), b1.calcStationVelocityInBody(state, point, matter.Ground()));
+    assertEqual(b1.findStationVelocityInGround(state, Vec3(0)), b1.getBodyOriginVelocity(state));
+    assertEqual(b1.findStationAccelerationInGround(state, Vec3(0)), b1.getBodyOriginAcceleration(state));
+    assertEqual(b1.findStationVelocityInGround(state, point), b1.findStationVelocityInAnotherBody(state, point, matter.Ground()));
 }
 
 int main() {
