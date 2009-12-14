dbb458a64bf0a5c4932127f1c9f8c46e8570db93
Michael Sherman <sherm1@gmail.com>
2009-12-14 22:52:03 +0000

    Merged down Thermostat and LinearBushing changes from trunk to 2.0 branch.

--- SimTKCoreRelease2.0/include/SimTKsimbody.h
+++ SimTKCoreRelease2.0/include/SimTKsimbody.h
@@ -37,6 +37,36 @@
  * Simbody capabilities.
  */
 
+/** @mainpage       Simbody Reference Documentation
+ *
+ * This is the Doxygen-generated reference material for Simbody objects. Until
+ * we finish this main page you'll have to look up the objects by their class
+ * names.
+ *
+ * @section main_intro Introduction
+ * Simbody is an API for building multibody systems, whose main components
+ * are:
+ *  - @ref main_mobod
+ *  - @ref main_forces
+ *  - @ref main_constraints
+ *  - @ref main_motions
+ *
+ * @section main_mobod Mobilized Bodies
+ * A MobilizedBody is a rigid body plus its mobilizer, which is the joint that 
+ * defines the body's mobility with respect to its parent body in the multibody
+ * tree. Here are some of the built-in mobilizer types:
+ * - @link SimTK::MobilizedBody::Pin    Pin (torsion, revolute) @endlink
+ * - @link SimTK::MobilizedBody::Ball   Ball (spherical) @endlink
+ * - @link SimTK::MobilizedBody::Slider Slider (prismatic) @endlink
+ * - @link SimTK::MobilizedBody::Free   Free (6 dof) @endlink
+ * @section main_forces Forces
+ * tbd
+ * @section main_constraints Constraints
+ * tbd
+ * @section main_motions Motions
+ * tbd
+ */
+
 #include "SimTKcommon.h"
 #include "SimTKmath.h"
 #include "simbody/internal/common.h"
@@ -49,6 +79,8 @@
 #include "simbody/internal/CollisionDetectionAlgorithm.h"
 #include "simbody/internal/ElasticFoundationForce.h"
 #include "simbody/internal/Force.h"
+#include "simbody/internal/Force_LinearBushing.h"
+#include "simbody/internal/Force_Thermostat.h"
 #include "simbody/internal/ForceSubsystem.h"
 #include "simbody/internal/SimbodyMatterSubsystem.h"
 #include "simbody/internal/SimbodyMatterSubtree.h"
