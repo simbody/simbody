448f2efa9bd01e936c02f996fa759dd2034747d8
Michael Sherman <sherm1@gmail.com>
2010-11-25 06:39:00 +0000

    Added sleepInNs() and sleepInSec() functions for high-precision thread blocking.

--- trunk/include/SimTKcommon/internal/Timing.h
+++ trunk/include/SimTKcommon/internal/Timing.h
@@ -37,35 +37,37 @@ clock_getttime() and nanosleep(), and also provides some convenient methods
 for use in common timing situations. **/
 
 /**@defgroup TimingFunctions Timing Functions
- * @ingroup GlobalFunctions
- *
- * These functions provide a convenient way to do timings, either for real
- * time use or performance measurement. Both elapsed timings and CPU timings
- * are supported, with the latter on a per-process or per-thread basis.
- * Times are returned as a double precision floating point number of seconds,
- * which is usually the most convenient form. Alternatives are available that
- * return timings as a 64 bit integer count of nanosecond ticks, providing
- * the highest resolution for very short measurements.
- *
- * Note that you can also use the Posix functions clock_gettime() and 
- * nanosleep() on any SimTK-supported platform.
- *
- * On Linux or Mac systems, use of any of these timing functions may require 
- * linking with the librt realtime library (-lrt).
- */
+   @ingroup GlobalFunctions
+
+These functions provide a convenient way to do high-precision timing, either 
+for real time use or performance measurement, and to sleep for precise time 
+intervals. Both elapsed and CPU timing is supported, with the latter on a 
+per-process or per-thread basis. Times are measured using a double precision
+floating point number of seconds, which is usually the most convenient form.
+Alternatives are available that measure time as a 64 bit integer count of 
+nanosecond ticks, providing the highest resolution and consistency for very 
+short measurements.
+
+Note that you can also use the Posix functions clock_gettime() and nanosleep() 
+on any SimTK-supported platform, as well as usleep() (time in microseconds)
+although Posix considers that obsolete.
+
+On Linux systems, use of these timing functions requires linking with the 
+librt realtime library (-lrt). **/
 
 #include "SimTKcommon/internal/common.h"
 #include "SimTKcommon/Constants.h"
 
-// This header is needed on Mac and Linux for the Posix time functions
-// and the timespec struct.
+// This header is needed on Mac and Linux for some or all of the Posix time 
+// functions and the timespec struct. We include it on Windows also for 
+// uniform cross-platform behavior, since there are many other useful time-
+// date-handling symbols declared here on all platforms.
 #include <ctime>
 
 #if defined(_MSC_VER)
     /* On Windows, the timespec struct is not defined. However, note that the 
-     * timespec struct is also defined in the pthread.h header on Windows, so
-     * the guard symbols must match here to avoid a duplicate.
-     */
+    timespec struct is also defined in the pthread.h header on Windows, so the 
+    guard symbols must match here to avoid a duplicate declaration. */
     #ifndef HAVE_STRUCT_TIMESPEC
     #define HAVE_STRUCT_TIMESPEC 1
     struct timespec {
@@ -74,20 +76,21 @@ for use in common timing situations. **/
     };
     #endif /* HAVE_STRUCT_TIMESPEC */
 
-    /* Posix nanosleep() sleeps the indicated number of nanoseconds and returns 0, or 
-     * if it is interrupted early it returns how much time was left in rem and returns
-     * EINTR. Ours is not interruptable so will always succeed and return rem==0. It is
-     * OK if rem is NULL, but req==NULL or req<0 returns EINVAL. A time of req==0 is
-     * allowed and our interpretation is that the thread relinquishes its time slice
-     * to another ready-to-run thread if there is one, otherwise returns immediately.
-     * This implementation rounds the desired sleep time to the nearest millisecond.
-     * On a Linux system, this requires including <time.h> (or <ctime>).
-     */
+    /* Posix nanosleep() sleeps the indicated number of nanoseconds and returns
+    0, or if it is interrupted early it returns how much time was left in 
+    rem and returns EINTR. Ours is not interruptable so will always succeed and
+    return rem==0. It is OK if rem is NULL, but req==NULL or req<0 returns 
+    EINVAL. A time of req==0 is allowed and our interpretation is that the 
+    thread relinquishes its time slice to another ready-to-run thread if there
+    is one, otherwise returns immediately. This implementation rounds the 
+    desired sleep time to the nearest millisecond. On a Linux system, this 
+    requires including <time.h> (or <ctime>), which we already included 
+    above. */
     SimTK_SimTKCOMMON_EXPORT int nanosleep(const struct timespec* req, struct timespec* rem);
 
-    /* Posix declares this handy function obsolete. It sleeps for the given number
-     * of microseconds.
-     */
+    /* Posix declares this handy function obsolete, but I don't think it is in
+    any danger of going away. It sleeps for the given number of microseconds.
+    However, using SimTK::sleepInNs() or SimTK::sleepInSec() is safer. */
     typedef unsigned int useconds_t;
     inline int usleep(useconds_t us) {
         struct timespec req;
@@ -99,32 +102,31 @@ for use in common timing situations. **/
 #endif
 
 #if defined(_MSC_VER) || defined(__APPLE__)
-    /* On Windows and OSX, the Posix clock_gettime function is missing.
-     */
+    // On Windows and OSX, the Posix clock_gettime function is missing.
     typedef long clockid_t;
 
-    /* These constants are the clock ids we support. All the varieties
-     * of CLOCK_MONOTONIC are high resolution with no NTP adjustments.
-     * I measured the resolutions on a single Windows 7 machine; hopefully
-     * they are typical (resolution here means how often they are updated):
-     *   - MONOTONIC (counter):    0.001ms      1us
-     *   - REALTIME (time of day):    1ms    1000us
-     *   - CPUTIME (either):         20ms   20000us
-     * These are slightly conservative resolutions so you should be able
-     * to achieve them in practice.
-     */
-    #define CLOCK_REALTIME              1   /* time of day clock, from 1/1/1970 */ 
-    #define CLOCK_MONOTONIC             2   /* counter from last boot time */
-    #define CLOCK_MONOTONIC_HR          3   /* "high resolution" (same) */
-    #define CLOCK_MONOTONIC_RAW         4   /* "not subject to NTP adjustments" (same) */
-    #define CLOCK_THREAD_CPUTIME_ID     5   /* lifetime cpu time (kernel+user) of the current thread */ 
-    #define CLOCK_PROCESS_CPUTIME_ID    6   /* cumulative cpu time of all threads of this process */
+    /* These constants are the clock ids we support. All the varieties of 
+    CLOCK_MONOTONIC are high resolution with no NTP adjustments. I measured 
+    the resolutions on a single Windows 7 machine; hopefully they are typical
+    (resolution here means how often they are updated):
+      - MONOTONIC (counter):    0.001ms      1us
+      - REALTIME (time of day):    1ms    1000us
+      - CPUTIME (either):         20ms   20000us
+    These are slightly conservative resolutions so you should be able to 
+    achieve them in practice. */
+    #define CLOCK_REALTIME           1 // time of day clock, from 1/1/1970
+    #define CLOCK_MONOTONIC          2 // counter from last boot time
+    #define CLOCK_MONOTONIC_HR       3 // "high resolution" (same)
+    #define CLOCK_MONOTONIC_RAW      4 // "not subject to NTP adjustments" (same)
+    #define CLOCK_THREAD_CPUTIME_ID  5 // current thread's cpu time (kernel+user)
+    #define CLOCK_PROCESS_CPUTIME_ID 6 // cumulative cpu time of all threads of
+                                       //   this process, live or dead
 
-    /* Returns zero if it succeeds (or if tp==NULL); otherwise EINVAL. 
-     * On a Linux system, this requires including <time.h> (or <ctime>)
-     * and linking with -lrt to get the realtime library.
-     */
-    SimTK_SimTKCOMMON_EXPORT int clock_gettime(clockid_t clock_id, struct timespec *tp); 
+    /* Returns zero if it succeeds (or if tp==NULL); otherwise EINVAL. On a 
+    Linux system, this requires including <time.h> (or <ctime>) and linking 
+    with -lrt to get the realtime library. */
+    SimTK_SimTKCOMMON_EXPORT int clock_gettime(clockid_t clock_id, 
+                                               struct timespec *tp); 
 #endif
 
 
@@ -162,11 +164,10 @@ inline long long timespecToNs(const timespec& ts)
 
 /** Given a signed number of nanoseconds, convert that into seconds and 
 leftover nanoseconds in a timespec struct. @see timespecToNs() **/
-inline void nsToTimespec(const long long& ns, timespec& ts) {
-    ts.tv_sec  = (long)(ns / 1000000000LL); // signed
+inline void nsToTimespec(const long long& ns, timespec& ts) 
+{   ts.tv_sec  = (long)(ns / 1000000000LL); // signed
     if (ns >= 0) ts.tv_nsec =  (long)(  ns  % 1000000000LL);
-    else         ts.tv_nsec = -(long)((-ns) % 1000000000LL);
-}
+    else         ts.tv_nsec = -(long)((-ns) % 1000000000LL); }
 
 /** Given a count of nanosecond ticks as a signed 64 bit integer, return
 the same time interval as a double precision floating point number of
@@ -185,7 +186,7 @@ inline long long secToNs(const double& s)
     @ingroup TimingFunctions
 
 These functions provide measurement of CPU time consumed by a process or
-by individual threads. Time included both kernel and user time together,
+by individual threads. Time includes both kernel and user time together,
 and is reported as a double precision floating point value in seconds.
 CPU timers typically have a very coarse resolution, likely to be in the 
 10-50ms range depending on the particulars of your system. That means you 
@@ -200,57 +201,51 @@ has been used so far by any of the threads in the currently executing process.
 @return CPU time used since this process was created, in seconds, as
 a double precision floating point number.
 @see threadCpuTime() **/
-inline double cpuTime() {
-    timespec ts;
+inline double cpuTime() 
+{   timespec ts;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts);
-    return (double)(timespecToNs(ts)*SimTK_NS_TO_S);
-}
+    return (double)(timespecToNs(ts)*SimTK_NS_TO_S); }
 
 /** Return the total CPU time in seconds (both kernel and user time) that 
 has been used so far by the currently executing thread.
 
 @return CPU time used since this thread was created, in seconds, as
 a double precision floating point number.
-@see cpuTime()
-**/
-inline double threadCpuTime() {
-    timespec ts;
+@see cpuTime() **/
+inline double threadCpuTime() 
+{   timespec ts;
     clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
-    return (double)(timespecToNs(ts)*SimTK_NS_TO_S);
-}
+    return (double)(timespecToNs(ts)*SimTK_NS_TO_S); }
 /**@}**/
 
-/** @defgroup ElapsedTime High-Resolution Elapsed Time Measurement
+/** @defgroup ElapsedTime High-Resolution Elapsed Time Measurement and Sleep
     @ingroup TimingFunctions
 
-These functions provide access to the system's high resolution interval
-timer, which measures elapsed time from some arbitrary starting point
-(typically since the system was last booted). It is expected that this
-timer provides very precise measurement of short time intervals, but
-cannot be depended upon to measure very long periods without drifting.
-That is, it may not be synchronized to the system time of day.
+These functions provide access to the system's high resolution interval timer,
+and highest precision sleep facility for unscheduling a thread until it is 
+needed at a specific later time. The interval timer measures elapsed time from
+some arbitrary starting point (typically since the system was last booted). It
+is expected that this timer provides very precise measurement of short time 
+intervals, but cannot be depended upon to measure very long periods without 
+drifting. That is, it may not be synchronized to the system time of day.
 
-Generally it is most convenient to measure intervals as a floating
-point number of seconds, however this provides a variable amount of
-precision as the absolute value of the timer increases. For maximum
-precision, you can obtain the timer value as an integer number of
-nanoseconds instead. You can improve accuracy by subtacting the integer 
-counts first before converting to seconds. The actual resolution is 
-system-dependent, but it should be able to accurately measure elapsed 
-times of 1ms or less, substantially less on some systems. **/
+Generally it is most convenient to measure intervals as a floating point number
+of seconds, however this provides a variable amount of precision as the 
+absolute value of the timer increases. For maximum precision, you can obtain 
+the timer value as an integer number of nanoseconds instead. You can improve
+accuracy by subtacting the integer counts first before converting to seconds. 
+The actual resolution is system-dependent, but it should be able to accurately
+measure elapsed times of 1ms or less, substantially less on some systems. **/
 /**@{**/
 
-/** Return current time on the high-resolution interval timer in
-nanoseconds, as a 64-bit integer count. Generally it is more convenient
-to use realTime() which reports the interval time in
-seconds instead, but the nanosecond count is best for maximum
-accuracy.
+/** Return current time on the high-resolution interval timer in nanoseconds, 
+as a 64-bit integer count. Generally it is more convenient to use realTime() 
+which reports the interval time in seconds instead, but the nanosecond count 
+is best for maximum accuracy and consistency.
 
-@return Elapsed nanoseconds since some arbitrary time, as a 64 bit 
-integer count.
-
-@see realTime(), nsToSec()
-**/
+@return Elapsed nanoseconds since some arbitrary time, as a 64 bit integer 
+count.
+@see realTime(), nsToSec() **/
 inline long long realTimeInNs() {
     timespec ts;
     #ifdef CLOCK_MONOTONIC_RAW
@@ -261,19 +256,35 @@ inline long long realTimeInNs() {
     return timespecToNs(ts);
 }
 
-/** Return current time on the high-resolution interval timer in
-seconds. For maximum precision, you can improve repeatability and 
-accuracy somewhat by obtaining the interval times as integer counts 
-using realTimeInNs(). 
+/** Return current time on the high-resolution interval timer in seconds. For
+maximum precision, you can improve repeatability and accuracy somewhat by 
+obtaining the interval times as integer counts using realTimeInNs(). 
 
-@return Elapsed seconds since some arbitrary time, as a double 
-precision floating point number.
+@return Elapsed seconds since some arbitrary time, as a double precision 
+floating point number.
+@see realTimeInNs() **/ 
+inline double realTime() {return nsToSec(realTimeInNs());}
 
-@see realTimeInNs()
-**/ 
-inline double realTime() {
-    return nsToSec(realTimeInNs());
-}
+/** Sleep for the indicated number of nanoseconds, with the actual precision
+system dependent but intended to be the best achievable, hopefully less than
+5ms in all cases. However, when this wakes up you should not assume you 
+reached the desired time; you could wake up a little earlier or a lot later.
+Alternatives to this are %SimTK's sleepInSec() function, or the Posix 
+nanosleep() and usleep() functions. 
+@see sleepInSec() **/
+inline void sleepInNs(const long long& ns) 
+{   timespec ts;
+    nsToTimespec(ns, ts);
+    nanosleep(&ts, 0); }
+
+/** Sleep for the indicated number of seconds, with the actual precision
+system dependent but intended to be the best achievable, hopefully less than
+5ms in all cases. However, when this wakes up you should not assume you reached
+the desired time; you could wake up a little earlier or a lot later. 
+Alternatives to this are the %SimTK sleepInNs() function, or the Posix 
+nanosleep() and usleep() functions. 
+@see sleepInNs() **/
+inline void sleepInSec(const double& seconds) {sleepInNs(secToNs(seconds));}
 /**@}**/
 
 } // namespace SimTK
