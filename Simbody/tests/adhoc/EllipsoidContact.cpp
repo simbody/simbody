32b68b26cae81ecea9e15189dd6bd1e369442025
Michael Sherman <sherm1@gmail.com>
2011-02-09 23:12:11 +0000

    - Switched ellipsoid test to use Paul Mitiguy's rattleback data. - Overload unary + for coordinate axes to allow +XAxis as well as -XAxis as directions.

--- Simbody/tests/adhoc/EllipsoidContact.cpp
+++ Simbody/tests/adhoc/EllipsoidContact.cpp
@@ -46,6 +46,12 @@ using namespace SimTK;
 
 Array_<State> saveEm;
 
+
+const Real Cm2m     = 1e-2;
+const Real CmSq2mSq = Cm2m*Cm2m; // conversion from sq cm to sq m
+const Real Deg2Rad  = (Real)SimTK_DEGREE_TO_RADIAN;
+const Real Rad2Deg  = (Real)SimTK_RADIAN_TO_DEGREE;
+
 static const Real TimeScale = 1;
 static const Real FrameRate = 30;
 static const Real ReportInterval = TimeScale/FrameRate;
@@ -136,6 +142,7 @@ public:
              << " E+Ediss=" << m_system.calcEnergy(state)
                                +m_compliant.getDissipatedEnergy(state)
              << endl;
+        cout << " q0(Deg): " << state.getQ()[0]*Rad2Deg << endl;
         const int ncont = m_compliant.getNumContactForces(state);
         cout << "Num contacts: " << m_compliant.getNumContactForces(state) << endl;
         for (int i=0; i < ncont; ++i) {
@@ -174,8 +181,6 @@ private:
     Visualizer::InputSilo& m_silo;
 };
 
-
-
 int main() {
   try
   { // Create the system.
@@ -183,57 +188,48 @@ int main() {
     MultibodySystem         system;
     SimbodyMatterSubsystem  matter(system);
     GeneralForceSubsystem   forces(system);
-    Force::Gravity   gravity(forces, matter, UnitVec3(0,-1,0), 9.8);
+    Force::Gravity   gravity(forces, matter, UnitVec3(-1,0,0), 9.81);
 
     ContactTrackerSubsystem  tracker(system);
     CompliantContactSubsystem contactForces(system, tracker);
-    contactForces.setTransitionVelocity(1e-3);
+    contactForces.setTransitionVelocity(1e-2); // m/s
 
-    ContactCliqueId clique1 = ContactSurface::createNewContactClique();
-    ContactCliqueId clique2 = ContactSurface::createNewContactClique();
-    ContactCliqueId clique3 = ContactSurface::createNewContactClique();
+    // Ground's normal is +x for this model
+    system.setUpDirection(+XAxis);
 
-    const Real fFac =1; // to turn off friction
-    const Real fDis = .5*0.2; // to turn off dissipation
-    const Real fVis =  .1*.1; // to turn off viscous friction
-    const Real fK = 100*1e6; // pascals
+    // Uncomment this if you want a more elegant movie.
+    //matter.setShowDefaultGeometry(false);
 
-    // Right hand wall
-    //matter.Ground().updBody().addDecoration(Vec3(.25+.01,0,0),
-    //    DecorativeBrick(Vec3(.01,4,8)).setColor(Gray).setOpacity(.2));
-    //matter.Ground().updBody().addContactSurface(Vec3(.25,0,0),
-    //    ContactSurface(ContactGeometry::HalfSpace(),
-    //                   ContactMaterial(fK*.01,fDis*.9,fFac*.8,fFac*.7,fVis*10))
-    //                   .joinClique(clique3));
+    const Real ud = .3; // dynamic
+    const Real us = .6; // static
+    const Real uv = 0;  // viscous (force/velocity)
+    const Real k = 1e8; // pascals
+    const Real c = 0.01; // dissipation (1/v)
 
-    // Halfspace floor
-    const Rotation R_xdown(-Pi/2,ZAxis);
-    //matter.Ground().updBody().addDecoration(
-    //    Transform(R_xdown, Vec3(0,-3-.01,0)),
-    //    DecorativeBrick(Vec3(.01,4,8)).setColor(Gray).setOpacity(.1));
+
+    // Halfspace default is +x, this one occupies -x instead, so flip.
+    const Rotation R_xdown(Pi,ZAxis);
+
     matter.Ground().updBody().addContactSurface(
         Transform(R_xdown, Vec3(0,0,0)),
         ContactSurface(ContactGeometry::HalfSpace(),
-                       ContactMaterial(fK*.1,fDis*.9,fFac*.8,fFac*.7,fVis*10))
-                       //ContactMaterial(2e6,.01,.1,.05,.01))
-                       .joinClique(clique1));
+                       ContactMaterial(k,c,us,ud,uv)));
 
 
-
-    const Real ellipsoidMass = 100;
-    const Vec3 halfDims(10, 2, 1);
-    Body::Rigid ellipsoidBody(MassProperties(ellipsoidMass, Vec3(0), 
-                              ellipsoidMass*UnitInertia::ellipsoid(halfDims)));
+    const Real ellipsoidMass = 1; // kg
+    const Vec3 halfDims(2*Cm2m, 20*Cm2m, 3*Cm2m); // m (read in cm)
+    const Vec3 comLoc(-1*Cm2m, 0, 0); 
+    const Inertia centralInertia(Vec3(17,2,16)*CmSq2mSq, Vec3(0,0,.2)*CmSq2mSq); // now kg-m^2
+    const Inertia inertia(centralInertia.shiftFromMassCenter(-comLoc, ellipsoidMass)); // in S
+    Body::Rigid ellipsoidBody(MassProperties(ellipsoidMass, comLoc, inertia));
 
     ellipsoidBody.addDecoration(Transform(), 
         DecorativeEllipsoid(halfDims).setColor(Cyan)
-         .setOpacity(.5).setResolution(10));
+         //.setOpacity(.5)
+         .setResolution(3));
     ellipsoidBody.addContactSurface(Transform(),
         ContactSurface(ContactGeometry::Ellipsoid(halfDims),
-                       ContactMaterial(fK*.1,fDis*.9,
-                                       .1*fFac*.8,.1*fFac*.7,fVis*1))
-                       //ContactMaterial(2e6,.01,.1,.05,.01))
-                       //.joinClique(clique2)
+                       ContactMaterial(k,c,us,ud,uv))
                        );
     MobilizedBody::Free ellipsoid(matter.Ground(), Transform(Vec3(0,0,0)),
         ellipsoidBody, Transform(Vec3(0)));
@@ -242,9 +238,8 @@ int main() {
     Visualizer viz(system);
     viz.addDecorationGenerator(new ForceArrowGenerator(system,contactForces));
     viz.setMode(Visualizer::RealTime);
-    viz.setDesiredBufferLengthInSec(1);
     viz.setDesiredFrameRate(FrameRate);
-    viz.setShowShadows(true);
+    viz.setCameraClippingPlanes(0.1, 10);
 
     Visualizer::InputSilo* silo = new Visualizer::InputSilo();
     viz.addInputListener(silo);
@@ -268,8 +263,16 @@ int main() {
     
     system.realizeTopology();
     State state = system.getDefaultState();
-    ellipsoid.setQToFitTransform(state, Transform(Rotation(),
-                                             Vec3(0,3,0)));
+    matter.setUseEulerAngles(state, true);
+    system.realizeModel(state);
+
+    ellipsoid.setQToFitTransform(state, Transform(
+        Rotation(BodyRotationSequence,  0  *Deg2Rad, XAxis,
+                                        0.5*Deg2Rad, YAxis,
+                                       -0.5*Deg2Rad, ZAxis),
+        Vec3(2.1*Cm2m, 0, 0)));
+
+    ellipsoid.setUToFitAngularVelocity(state, 2*Vec3(5,0,0)); // rad/s 
 
     viz.report(state);
     printf("Default state\n");
@@ -281,8 +284,7 @@ int main() {
              cout << "\aDude ... follow instructions!\n";
     } while (menuId != RunMenuId || item != GoItem);
 
-    ellipsoid.setOneU(state, 1, .1);
-    ellipsoid.setOneU(state, 0, .05); 
+
     
     // Simulate it.
 
@@ -293,7 +295,7 @@ int main() {
     //RungeKutta3Integrator integ(system);
     //VerletIntegrator integ(system);
     //integ.setMaximumStepSize(1e-0001);
-    integ.setAccuracy(1e-3); // minimum for CPodes
+    integ.setAccuracy(1e-4); // minimum for CPodes
     //integ.setAccuracy(.01);
     TimeStepper ts(system, integ);
 
@@ -302,7 +304,7 @@ int main() {
     double cpuStart = cpuTime();
     double realStart = realTime();
 
-    ts.stepTo(20.0);
+    ts.stepTo(10.0);
 
     const double timeInSec = realTime() - realStart;
     const int evals = integ.getNumRealizations();
