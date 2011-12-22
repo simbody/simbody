aea4faa80a579122b878a2bfd33c5fcd4e8eca13
Michael Sherman <sherm1@gmail.com>
2011-12-22 19:41:53 +0000

    Move geometry tests from Simbody to Simmath.

--- SimTKmath/tests/TestTriangleMesh.cpp
+++ SimTKmath/tests/TestTriangleMesh.cpp
@@ -29,7 +29,7 @@
  * USE OR OTHER DEALINGS IN THE SOFTWARE.                                     *
  * -------------------------------------------------------------------------- */
 
-#include "SimTKsimbody.h"
+#include "SimTKmath.h"
 #include <vector>
 #include <exception>
 
