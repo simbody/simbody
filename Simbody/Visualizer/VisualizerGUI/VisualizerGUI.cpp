c36471b02199fbff9fd1662df590c3d2f14d0a52
Michael Sherman <sherm1@gmail.com>
2012-03-12 04:05:14 +0000

    Fixed two long-standing problems with the Visualizer that could cause intermittent crashes: - When drawing in GL_POINTS mode, the number of points given was 3 times the correct number, causing random points to display and crashes. - When the mouse is clicked before the first scene is assembled, it would reference a null scene.

--- Simbody/Visualizer/VisualizerGUI/VisualizerGUI.cpp
+++ Simbody/Visualizer/VisualizerGUI/VisualizerGUI.cpp
@@ -152,7 +152,8 @@ static void computeBoundingSphereForVertices(const vector<float>& vertices, floa
 
 class Mesh {
 public:
-    Mesh(vector<float>& vertices, vector<float>& normals, vector<GLushort>& faces) : numVertices((int)(vertices.size()/3)), faces(faces) {
+    Mesh(vector<float>& vertices, vector<float>& normals, vector<GLushort>& faces) 
+    :   numVertices((int)(vertices.size()/3)), faces(faces) {
         // Build OpenGL buffers.
 
         GLuint buffers[2];
@@ -192,7 +193,7 @@ public:
         if (representation == DecorativeGeometry::DrawSurface)
             glDrawElements(GL_TRIANGLES, (GLsizei)faces.size(), GL_UNSIGNED_SHORT, &faces[0]);
         else if (representation == DecorativeGeometry::DrawPoints)
-            glDrawArrays(GL_POINTS, 0, numVertices*3);
+            glDrawArrays(GL_POINTS, 0, numVertices);
         else if (representation == DecorativeGeometry::DrawWireframe)
             glDrawElements(GL_LINES, (GLsizei)edges.size(), GL_UNSIGNED_SHORT, &edges[0]);
     }
@@ -1598,6 +1599,9 @@ static float sceneScale; // for scaling translations
 // function handles those properly if they are present.
 static void mouseButtonPressedOrReleased(int button, int state, int x, int y) {
     const int GlutWheelUp = 3, GlutWheelDown = 4;
+
+    if (scene == NULL)
+        return; // probably mouse click during startup
 
     float radius;
     fVec3 sceneCenter;
