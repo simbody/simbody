7bedcf10bc6acdac5f68049d4871b0ae5b9c3ea5
Michael Sherman <sherm1@gmail.com>
2013-06-03 16:31:47 +0000

    Fix some doxygen comments.

--- SimTKmath/Integrators/include/simmath/SemiExplicitEuler2Integrator.h
+++ SimTKmath/Integrators/include/simmath/SemiExplicitEuler2Integrator.h
@@ -48,15 +48,16 @@ setting.
 
 See SemiExplicitEulerIntegrator for the theory behind the underlying first
 order method. Here we use Richardson Extrapolation (step doubling) to get an
-error estimate and higher order result. See Solving Ordinary Differential
-Equations I: Nonstiff Problems, 2nd ref. ed., Hairer, Norsett, & Wanner,
-section II.4, pp 164-165.
+error estimate. See Solving Ordinary Differential Equations I: Nonstiff 
+Problems, 2nd rev. ed., Hairer, Norsett, & Wanner, section II.4, pp 164-165.
+Note that we are not using the local extrapolation trick to get a higher-order
+result; see below for why.
 
 The idea is to take two steps of length h, and a single big step of length 2h, 
 and combine the results. Starting at t=t0 we can get to t2=t0+2h two different
 ways: <pre>
     u1 = u0 + h udot(q0,u0)              ub = u0 + 2h udot(q0,u0)
-    q1 = q0 + h N(q0)u1         and      qb = q0 + 2h N(q0)u2'
+    q1 = q0 + h N(q0)u1         and      qb = q0 + 2h N(q0)ub
     u2 = u1 + h udot(q1,u1)                  (b = big step)
     q2 = q1 + h N(q1)u2
 </pre> Note that we can use udot(q0,u0) in both methods so it doesn't cost much
@@ -71,17 +72,16 @@ Now assume the true solution at t2 is y(t2)=(q,u). Then <pre>
 </pre> which is the 2nd order error term for y2. We can use that for error 
 control. 
 
-Hairer suggests using that error term to improve on y2 to
-get a better estimate for y (this is called "local extrapolation") : <pre>
+Hairer suggests using that error term to improve on y2 to get a better estimate 
+for y (this is called "local extrapolation") : <pre>
     y2' = y2 + (y2-yb) = 2 y2 - yb
     y   = y2' + O(h^3)                  <-- we don't actually do this
 </pre> y2' is a 2nd order accurate estimate for y(t2). But ... we don't have an
 error estimate for the revised value and we don't actually know that it is more
-accurate. In fact, although this sounds
-lovely in theory, in practice we will often be running at close to a stability 
-boundary and using the "big step" value to update the result causes stability
-problems. So we'll skip the local extrapolation and stick with the first order
-result from the two half-steps.
+accurate. In fact, although this sounds lovely in theory, in practice we will 
+often be running at close to a stability boundary and using the "big step" value
+to update the result causes stability problems. So we'll skip the local 
+extrapolation and stick with the first order result from the two half-steps.
 
 @author Michael Sherman
 **/
