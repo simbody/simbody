2a13a1ab16d847bf0c3a09f41265847431d5ba69
Christopher Dembia <cld72@cornell.edu>
2013-11-27 19:54:51 -0800

    Follow the disc with camera and make it spin.
    I tried to follow @sherm1's advice about how to set the initial speed of the
    wheel. Currently there may be more damping than we want. Also, I am trying to
    follow the disc with the camera. However, I cannot seem to call both
    setCameraTransform() and pointCameraAt(); only one or the other can be
    satisfied.

--- Simbody/examples/ExampleRollingDisc.cpp
+++ Simbody/examples/ExampleRollingDisc.cpp
@@ -34,15 +34,33 @@ is of a zero-height disc.  The disc rolls in the x direction of the yaw
 frame/body.
 */
 
+/// We want the camera to follow the disc.
+class FollowDisc : public Visualizer::FrameController
+{
+public:
+    void generateControls(const Visualizer & viz, const State & state,
+            Array_<DecorativeGeometry> & geometry)
+    {
+        Vector q = state.getQ();
+        std::cout << q << std::endl;
+        // I figured the proper indices by guess and check.
+        double x = q[1];
+        double z = q[3];
+        viz.setCameraTransform(Transform(Vec3(0, 5, x + 5.0)));
+        viz.pointCameraAt(Vec3(x, 0, z), Vec3(0, 1, 0));
+    }
+};
+
 int main() {
 
     // TODO Explain the degrees of freedom (transforms for mobilized bodies).
 
     // Parameters.
     // -----------
+    // The disc is about the size of a coin.
     double radius = 1.0;
-    double height = 0.01;
-    double mass = 1.0;
+    double height = 0.01 * radius;
+    double mass = 0.005;
 
     // We'll use this a lot.
     Vec3 rvec(0, 0, radius);
@@ -93,26 +111,34 @@ int main() {
     // Constrain the disc to roll.
     // ---------------------------
     // Along the direction of travel.
+    // The contact point is fixed in the leanMB frame.
     Constraint::NoSlip1D(leanMB, Vec3(0), UnitVec3(1, 0, 0),
             matter.updGround(), discMB);
     // Perpendicular to the direction of travel.
-    // TODO this is likely wrong.
     Constraint::NoSlip1D(leanMB, Vec3(0), UnitVec3(0, 1, 0),
             matter.updGround(), discMB);
+
+    // We want the disc to slow down so that it falls over.
+    Force::MobilityLinearDamper(forces, discMB, MobilizerUIndex(0), 0.2);
 
     // Visualize.
     // ----------
     Visualizer viz(system);
+    viz.addFrameController(new FollowDisc());
     // Report at the framerate (real-time).
     system.addEventReporter(new Visualizer::Reporter(viz, 1./30));
 
     // Initialize the system and state.
     // --------------------------------
     State state = system.realizeTopology();
-    // So the disc starts off spinning; 1 rotation per second.
-    discMB.setRate(state, 20.*Pi);
+
+    // We want the disc to start off spinning, in the +x direction.
+    discMB.setRate(state, -2 * Pi);
+    leanMB.setAngle(state, 0.1*Pi);
+    discMB.lock(state, Motion::Velocity);
+    system.project(state);
+    discMB.unlock(state);
     // So the disc falls over.
-    leanMB.setAngle(state, 0.01*Pi);
 
     // Simulate the disc.
     // ------------------
