9bf2a728e3207830d075c6897addab15ae4df693
Michael Sherman <msherman@stanford.edu>
2014-01-30 20:34:57 -0800

    Fix flop count in comment to reflect fix to calculation of getTotalCentrifugalForce() (it's cheaper now).

--- Simbody/src/RigidBodyNode.cpp
+++ Simbody/src/RigidBodyNode.cpp
@@ -199,7 +199,7 @@ Real RigidBodyNode::calcKineticEnergy(
 // This routine expects that coriolis accelerations, gyroscopic forces, and
 // articulated body inertias are already available.
 // As written, the calling order doesn't matter.
-// Cost is 144 flops.
+// Cost is 117 flops.
 void 
 RigidBodyNode::calcJointIndependentDynamicsVel(
     const SBTreePositionCache&              pc,
@@ -217,7 +217,7 @@ RigidBodyNode::calcJointIndependentDynamicsVel(
     updMobilizerCentrifugalForces(dc) =
         getP(abc) * getMobilizerCoriolisAcceleration(vc) + getGyroscopicForce(vc);
 
-    // 72 flops
+    // 45 flops
     updTotalCentrifugalForces(dc) = 
         getMk_G(pc) * getTotalCoriolisAcceleration(vc) + getGyroscopicForce(vc);
 }
