e03aa744f846c577ca770a137045231253d72f8c
Christopher Dembia <cld72@cornell.edu>
2014-02-14 15:30:30 -0800

    Remove SimTK:: qualification for gcc++11.

--- SimTKcommon/SmallMatrix/src/SmallMatrix.cpp
+++ SimTKcommon/SmallMatrix/src/SmallMatrix.cpp
@@ -44,26 +44,26 @@ template class Mat< 3,3,conjugate<float>,2 >;
 template class SymMat< 4, std::complex<double>, 7>;
 
 
-template Real       SimTK::det(const Mat<1,1,Real>&);
-template Real       SimTK::det(const SymMat<1,Real>&);
-template Complex    SimTK::det(const Mat<2,2,Complex>&);
-template Real       SimTK::det(const SymMat<2,Real>&);
-template Real       SimTK::det(const Mat<3,3,Real>&);
-template Real       SimTK::det(const SymMat<3,Real>&);
-template Real       SimTK::det(const Mat<5,5,Real>&);
-template Complex    SimTK::det(const SymMat<5,Complex>&);
+template Real       det(const Mat<1,1,Real>&);
+template Real       det(const SymMat<1,Real>&);
+template Complex    det(const Mat<2,2,Complex>&);
+template Real       det(const SymMat<2,Real>&);
+template Real       det(const Mat<3,3,Real>&);
+template Real       det(const SymMat<3,Real>&);
+template Real       det(const Mat<5,5,Real>&);
+template Complex    det(const SymMat<5,Complex>&);
 
-template Mat<1,1,Real>::TInvert      SimTK::lapackInverse(const Mat<1,1,Real>&);
-template Mat<2,2,Real>::TInvert      SimTK::lapackInverse(const Mat<2,2,Real>&);
-template Mat<3,3,Conjugate>::TInvert SimTK::lapackInverse(const Mat<3,3,Conjugate>&);
+template Mat<1,1,Real>::TInvert      lapackInverse(const Mat<1,1,Real>&);
+template Mat<2,2,Real>::TInvert      lapackInverse(const Mat<2,2,Real>&);
+template Mat<3,3,Conjugate>::TInvert lapackInverse(const Mat<3,3,Conjugate>&);
 
-template Mat<1,1,Real>::TInvert     SimTK::inverse(const Mat<1,1,Real>&);
-template SymMat<1,Real>::TInvert    SimTK::inverse(const SymMat<1,Real>&);
-template Mat<2,2,Complex>::TInvert  SimTK::inverse(const Mat<2,2,Complex>&);
-template SymMat<2,Real>::TInvert    SimTK::inverse(const SymMat<2,Real>&);
-template Mat<3,3,Real>::TInvert     SimTK::inverse(const Mat<3,3,Real>&);
-template SymMat<3,Real>::TInvert    SimTK::inverse(const SymMat<3,Real>&);
-template Mat<5,5,Real>::TInvert     SimTK::inverse(const Mat<5,5,Real>&);
+template Mat<1,1,Real>::TInvert     inverse(const Mat<1,1,Real>&);
+template SymMat<1,Real>::TInvert    inverse(const SymMat<1,Real>&);
+template Mat<2,2,Complex>::TInvert  inverse(const Mat<2,2,Complex>&);
+template SymMat<2,Real>::TInvert    inverse(const SymMat<2,Real>&);
+template Mat<3,3,Real>::TInvert     inverse(const Mat<3,3,Real>&);
+template SymMat<3,Real>::TInvert    inverse(const SymMat<3,Real>&);
+template Mat<5,5,Real>::TInvert     inverse(const Mat<5,5,Real>&);
 //template SymMat<5,Complex>::TInvert SimTK::inverse(const SymMat<5,Complex>&);
 
 } //namespace SimTK
