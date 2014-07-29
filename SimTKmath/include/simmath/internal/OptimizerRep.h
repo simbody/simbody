869c77dba4c44aa0c50fc2d18ed24b95448b45b0
Christopher Dembia <cld72@cornell.edu>
2014-07-28 17:38:07 -0700

    Implement virtual OptimizerRep::getAlgorithm()
    Previously, optimizers were required to define getAlgorithm(). However, if a user defines their own optimizer (e.g., CMA), they are unable to modify the OptimizerAlgorithm enum, and thus are not equipped to override getAlgorithm(). Thus, we must provide a definition for this method.

--- SimTKmath/include/simmath/internal/OptimizerRep.h
+++ SimTKmath/include/simmath/internal/OptimizerRep.h
@@ -153,7 +153,10 @@ public:
         return *jacDiff;
     }
 
-    virtual OptimizerAlgorithm getAlgorithm() const = 0;
+    virtual OptimizerAlgorithm getAlgorithm() const {
+        SimTK_THROW2(Exception::UnimplementedVirtualMethod,
+            "OptimizerRep", "getAlgorithm");
+    }
 
     static int numericalGradient_static( const OptimizerSystem&, const Vector & parameters,  const bool new_parameters,  Vector &gradient );
     static int numericalJacobian_static(const OptimizerSystem&,
