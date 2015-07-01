471e425519727384c4f1a90c8ffd1cb489397ba1
mjhmilla <millard.matthew@gmail.com>
2015-07-01 20:44:08 +0200

    Updated doxygen following Sherms feedback

--- Simbody/include/simbody/internal/SimbodyMatterSubsystem.h
+++ Simbody/include/simbody/internal/SimbodyMatterSubsystem.h
@@ -206,7 +206,7 @@ geometry that can be used to visualize this multibody system. **/
 bool getShowDefaultGeometry() const;
 
 /** The number of bodies includes all mobilized bodies \e including Ground,
-which is the 1st mobilized body, at the 0th MobilizedBodyIndex. (Note: if 
+which is the first mobilized body, at MobilizedBodyIndex 0. (Note: if 
 special particle handling were implemented, the count here would \e not 
 include particles.) Bodies and their inboard mobilizers have the same index 
 since they are grouped together as a MobilizedBody. MobilizedBody numbering 
@@ -217,11 +217,11 @@ you could think of its mobilizer as the Weld joint that attaches it to the
 universe), but otherwise every mobilized body has a unique body and mobilizer. 
 
 Example: 
-  \li Here we use a model of a planar pendulum with 1 degree-of-freedom.
-  \li The command getNumBodies() will return 2.
-  \li Why? Ground counts as a mobilized body, and so does the pendulum.
-  \li But note that Ground is at MobilizedBodyIndex(0), and the pendulum 
-      is at the MobilizedBodyIndex(1)**/
+  - Here we use a model of a planar pendulum with 1 degree-of-freedom.
+  - The command getNumBodies() will return 2  
+  - Why? Ground counts as a mobilized body, and so does the pendulum. 
+  - Note that Ground is at MobilizedBodyIndex(0), and the pendulum is at 
+    the MobilizedBodyIndex(1)**/
 int getNumBodies() const;
 
 /** This is the total number of defined constraints, each of which may
