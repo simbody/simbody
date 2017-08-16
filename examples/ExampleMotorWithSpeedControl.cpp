ccb648e3fc6ed1dbc0b75d018a70cf3f1c013e8c
xjing97 <vasanta97@gmail.com>
2017-08-16 14:04:59 +0800

    Update ExampleMotorWithSpeedControl.cpp

--- examples/ExampleMotorWithSpeedControl.cpp
+++ examples/ExampleMotorWithSpeedControl.cpp
@@ -350,5 +350,9 @@ int main() {
         std::cout << "ERROR: " << e.what() << std::endl;
         return 1;
     }
+     catch (...) {
+      std::cout << "UNKNOWN EXCEPTION\n";
+      return 1;
+  }
     return 0;
 }
