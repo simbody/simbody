3d83174ad2122627d70400a2b1a29ca1c9903c7c
Christopher Dembia <cld72@cornell.edu>
2017-09-27 16:32:08 -0700

    Remove usages of AtomicInteger.

--- Simbody/include/simbody/internal/ContactSurface.h
+++ Simbody/include/simbody/internal/ContactSurface.h
@@ -453,7 +453,7 @@ static bool cliquesIntersect(const Array_<ContactCliqueId,short>& a,
 safe). Every contact surface is automatically a member of a clique containing 
 all the surfaces that reside on the same body. **/
 static ContactCliqueId createNewContactClique()
-{   static AtomicInteger nextAvailableContactClique = 1;
+{   static std::atomic<int> nextAvailableContactClique(1);
     return ContactCliqueId(nextAvailableContactClique++); }
 
 /** For selection or other purposes, you may want to use this method to store
