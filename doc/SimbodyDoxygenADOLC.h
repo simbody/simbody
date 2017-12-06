9bed28f1966a1155adaed3d84128b32291d5561a
Christopher Dembia <cld72@cornell.edu>
2017-12-06 12:13:04 -0800

    Add SimbodyDoxygenADOLC.h

---
+++ doc/SimbodyDoxygenADOLC.h
@@ -0,0 +1,24 @@
+#ifndef SimTK_SIMBODY_DOXYGEN_ADOLC_H_
+#define SimTK_SIMBODY_DOXYGEN_ADOLC_H_
+
+/** @file
+This "header" file is actually just the source for Simbody's Doxygen
+page on ADOLC. This is not actually included as part of the
+Simbody source and it is not installed with Simbody. **/
+
+/** @page simbody_adolc_page  Automatic differentiation with ADOL-C
+
+It is possible to build Simbody's libraries with support for automatic
+differentiation, which is a way to obtain exact derivatives for a piece of code
+without needing to analytically derive the derivative yourself.
+
+TODO
+
+*/
+
+// This is so Doxygen can locate the symbols we mention.
+namespace SimTK {
+
+} // namespace SimTK
+
+#endif // SimTK_SIMBODY_DOXYGEN_ADOLC_H_
