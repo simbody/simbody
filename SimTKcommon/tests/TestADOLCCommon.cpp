e8d2351c19191de011309092bf42c1197249c91e
antoinefalisse <antoinefalisse@gmail.com>
2018-01-15 12:55:29 -0500

    attempt solving travis issue with gcc

--- SimTKcommon/tests/TestADOLCCommon.cpp
+++ SimTKcommon/tests/TestADOLCCommon.cpp
@@ -146,19 +146,19 @@ void testNegator() {
     adouble x;
     adouble y;
     x <<= xp[0];
-    y = (negator<adouble>&)x;;
+    y = (negator<adouble>&)NTraits<adouble>::pow(x,3);
     double y0;
     y >>= y0;
     trace_off();
     // function evaluation
     double f[1];
     function(2, 1, 1, xp, f);
-    SimTK_TEST(f[0] == -2.);
+    SimTK_TEST(f[0] == -8.);
     // derivative evaluation
     double** J;
     J = myalloc(1, 1);
     jacobian(2, 1, 1, xp, J);
-    SimTK_TEST(J[0][0] == -1);
+    SimTK_TEST(J[0][0] == -3*NTraits<adouble>::pow(x,2));
     myfree(J);
     // isNumericallyEqual
     adouble xd = 9.45;
