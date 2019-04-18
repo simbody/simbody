04a6d5ef8fe0841bfa81c7bb506380de02d51a07
Antoine Falisse <antoinefalisse@gmail.com>
2019-04-18 17:51:48 +0200

    add comments

--- Simbody/include/simbody/internal/HuntCrossleyForce_smooth.h
+++ Simbody/include/simbody/internal/HuntCrossleyForce_smooth.h
@@ -40,7 +40,7 @@ class HuntCrossleyForceImpl_smooth;
  * model available in Simbody. The proposed implementation differs from the
  * original implementation as it does not rely on a GeneralContactSubsystem.
  * Instead, it assumes contact between a sphere and a fixed half space that
- * can have an offset and an inclinationi with respect to the ground.
+ * can have an offset and a slope with respect to the ground.
  *
  * This smooth contact model was primarily developed for use in gradient-based
  * optimization (e.g., in predictive simulations of walking to simulate the
@@ -72,60 +72,50 @@ class HuntCrossleyForceImpl_smooth;
  * off the coefficient of restitution-vs.-velocity curves: it is the absolute
  * value of the slope at low velocities.
  *
- * Given a collision between a sphere and a plane, we can generate a contact
- * force from this equation
- *      f = kx^n(1 + 3/2 cv)
- * where k is a stiffness constant incorporating material properties
- * and geometry (to be defined below), x is penetration depth and
- * v = dx/dt is penetration rate (positive during penetration and
- * negative during rebound). Exponent n depends on the surface
- * geometry. For Hertz contact where the geometry can be approximated
- * by sphere (or sphere-plane) interactions, which is all we are
- * currently handling here, n=3/2.
+ * The original Hertz force between a sphere and a plane is given by:
+ *      fh = 4/3*k*x*(R*k*x)^(1/2)
+ * where k = 0.5*stiffness^(2/3) where stiffness is the effective Young's
+ * modulus, which is assumed identic for both contacting materials (i.e.,
+ * sphere and plane), x is penetration depth, and R is sphere radius.
+ * In the smooth approximation, we use the expression:
+ *      fh_pos = 4/3*k*(R*k*)^(1/2)*((x^2+eps)^(1/2)^(3/2))
+ *      fh_smooth = fh_pos*(1./2.+(1./2.)*tanh(bd*x));
+ * where eps=1e-5 enforces a small force even when there is no contact between
+ * the sphere and the plane and bd=300 determines the smoothness of the tanh
+ * transition.
  *
- * Stiffness k is defined in terms of the relative radius of curvature R and
- * effective plane-strain modulus E, each of which is a combination of
- * the description of the two individual contacting elements:
- *
- * <pre>
- *          R1*R2                                         E2^(2/3)
- *     R = -------,  E = (s1 * E1^(2/3))^(3/2),  s1= -------------------
- *         R1 + R2                                   E1^(2/3) + E2^(2/3)
- * </pre>
- *
- *     c = c1*s1 + c2*(1-s1)
- *     k = (4/3) sqrt(R) E
- *     f = k x^(3/2) (1 + 3/2 c xdot)
- *     pe = 2/5 k x^(5/2)
- * Also, we can calculate the contact patch radius a as
- *     a = sqrt(R*x)
- *
- * In the above, E1 and E2 are the *plane strain* moduli. If you have instead
- * Young's modulus Y1 and Poisson's ratio p1, then E1=Y1/(1-p1^2). The interface
- * to this subsystem asks for E1 (pressure/%strain) and c1 (1/velocity), and
- * E2,c2 only.
+ * The original Hunt-Crossley force is given by:
+ *      f = fh*(1+3/2*c*v)
+ * where c is dissipation and v is penetration rate.
+ * In the smooth approximation, we use the expression:
+ *      f_pos = fh_smooth*(1.+(3./2.)*c*v);
+ *      f_smooth = f_pos*(1./2.+(1./2.)*tanh(bv*(v+(2./(3.*v)))));
+ * where bv=50 determines the smoothness of the tanh transition.
  *
  * <h1>Friction Force</h1>
  *
  * The friction force is based on a model by Michael Hollars:
  *
- * f = fn*[min(vs/vt,1)*(ud+2(us-ud)/(1+(vs/vt)^2))+uv*vs]
+ * f = f_smooth*[min(vs/vt,1)*(ud+2(us-ud)/(1+(vs/vt)^2))+uv*vs]
  *
- * where fn is the normal force at the contact point, vs is the slip (tangential)
- * velocity of the two bodies at the contact point, vt is a transition velocity
- * (see below), and us, ud, and uv are the coefficients of static, dynamic, and
- * viscous friction respectively.  Each of the three friction coefficients is calculated
- * based on the friction coefficients of the two bodies in contact:
+ * where f_smooth is the smooth normal force at the contact point, vs is the
+ * slip (tangential) velocity of the two bodies at the contact point, vt is a
+ * transition velocity (see below), and us, ud, and uv are the coefficients of
+ * static, dynamic, and viscous friction respectively. Each of the three
+ * friction coefficients is calculated based on the friction coefficients of
+ * the two bodies in contact:
+ *      u = 2*u1*u2/(u1+u2)
+ * In the smooth approximation, we assume the same coefficients for both
+ * contacting materials.
  *
- * u = 2*u1*u2/(u1+u2)
- *
- * Because the friction force is a continuous function of the slip velocity, this
- * model cannot represent stiction; as long as a tangential force is applied, the
- * two bodies will move relative to each other.  There will always be a nonzero
- * drift, no matter how small the force is.  The transition velocity vt acts as an
- * upper limit on the drift velocity.  By setting vt to a sufficiently small value,
- * the drift velocity can be made arbitrarily small, at the cost of making the
- * equations of motion very stiff.  The default value of vt is 0.01.
+ * Because the friction force is a continuous function of the slip velocity,
+ * this model cannot represent stiction; as long as a tangential force is
+ * applied, the two bodies will move relative to each other. There will always
+ * be a nonzero drift, no matter how small the force is. The transition
+ * velocity vt acts as an upper limit on the drift velocity. By setting vt to a
+ * sufficiently small value, the drift velocity can be made arbitrarily small,
+ * at the cost of making the equations of motion very stiff. The default value
+ * of vt is 0.01.
  */
 class SimTK_SIMBODY_EXPORT HuntCrossleyForce_smooth : public Force {
 public:
@@ -136,13 +126,13 @@ public:
      */
     HuntCrossleyForce_smooth(GeneralForceSubsystem& forces);
     /**
-     * Set the material parameters for a surface in the contact set.
+     * Set the material parameters
      *
-     * @param stiffness       the stiffness constant (k) for the body
-     * @param dissipation     the dissipation coefficient (c) for the body
-     * @param staticFriction  the coefficient of static friction (us) for the body
-     * @param dynamicFriction the coefficient of dynamic friction (ud) for the body
-     * @param viscousFriction the coefficient of viscous friction (uv) for the body
+     * @param stiffness       the stiffness constant (k)
+     * @param dissipation     the dissipation coefficient (c)
+     * @param staticFriction  the coefficient of static friction (us)
+     * @param dynamicFriction the coefficient of dynamic friction (ud)
+     * @param viscousFriction the coefficient of viscous friction (uv)
      */
     void setParameters(Real stiffness, Real dissipation, Real staticFriction,
        Real dynamicFriction, Real viscousFriction, Real transitionVelocity);
