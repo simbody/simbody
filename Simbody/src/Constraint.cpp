120fd606439f1cba6d1c9d2d01855bb02a94f6db
Yun Tang <yun.tang@yahoo.com>
2019-08-21 13:39:12 -0400

    Remove tab in code

--- Simbody/src/Constraint.cpp
+++ Simbody/src/Constraint.cpp
@@ -2740,62 +2740,62 @@ addInPositionConstraintForces
 //==============================================================================
 Constraint::PrescribedVelocity::PrescribedVelocity
    (SimbodyMatterSubsystem&     matter,
-	const Function*             function,
-	MobilizedBodyIndex          speedBody,
-	MobilizerUIndex             speedIndex)
+    const Function*             function,
+    MobilizedBodyIndex          speedBody,
+    MobilizerUIndex             speedIndex)
 :   Custom(new PrescribedVelocityImpl(matter, function, speedBody, speedIndex)) {}
 
 Constraint::PrescribedVelocityImpl::PrescribedVelocityImpl
    (SimbodyMatterSubsystem& matter,
-	const Function* function,
-	MobilizedBodyIndex speedBody,
-	MobilizerUIndex speedIndex)
+    const Function* function,
+    MobilizedBodyIndex speedBody,
+    MobilizerUIndex speedIndex)
 :   Implementation(matter, 0, 1, 0), function(function),
-	speedIndex(speedIndex), temp(1), referenceCount(new int[1])
+    speedIndex(speedIndex), temp(1), referenceCount(new int[1])
 {
-	assert(function->getArgumentSize() == 1);
-	assert(function->getMaxDerivativeOrder() >= 2);
+    assert(function->getArgumentSize() == 1);
+    assert(function->getMaxDerivativeOrder() >= 2);
 
-	referenceCount[0] = 1;
-	const MobilizedBody& mobod = matter.getMobilizedBody(speedBody);
-	this->speedBody = addConstrainedMobilizer(mobod);
+    referenceCount[0] = 1;
+    const MobilizedBody& mobod = matter.getMobilizedBody(speedBody);
+    this->speedBody = addConstrainedMobilizer(mobod);
 }
 
 void Constraint::PrescribedVelocityImpl::
 calcVelocityErrors
-   (const State&                                    s,
-	const Array_<SpatialVec, ConstrainedBodyIndex>&  V_AB,
-	const Array_<Real, ConstrainedUIndex>&     constrainedU,
-	Array_<Real>&                                   verr) const
+   (const State&                                     s,
+    const Array_<SpatialVec, ConstrainedBodyIndex>&  V_AB,
+    const Array_<Real, ConstrainedUIndex>&           constrainedU,
+    Array_<Real>&                                    verr) const
 {
-	temp[0] = s.getTime();
-	verr[0] = getOneU(s, constrainedU, speedBody, speedIndex) 
-		      - function->calcValue(temp);
+    temp[0] = s.getTime();
+    verr[0] = getOneU(s, constrainedU, speedBody, speedIndex) 
+              - function->calcValue(temp);
 }
 
 void Constraint::PrescribedVelocityImpl::
 calcVelocityDotErrors
-   (const State&                                    s,
-	const Array_<SpatialVec, ConstrainedBodyIndex>&  A_AB,
-	const Array_<Real, ConstrainedUIndex>&     constrainedUDot,
-	Array_<Real>&                                   vaerr) const
+   (const State&                                     s,
+    const Array_<SpatialVec, ConstrainedBodyIndex>&  A_AB,
+    const Array_<Real, ConstrainedUIndex>&           constrainedUDot,
+    Array_<Real>&                                    vaerr) const
 {
-	temp[0] = s.getTime();
-	Array_<int> components(1, 0); // i.e., components={0}
-	vaerr[0] = getOneUDot(s, constrainedUDot, speedBody, speedIndex)
-		       - function->calcDerivative(components, temp);
+    temp[0] = s.getTime();
+    Array_<int> components(1, 0); // i.e., components={0}
+    vaerr[0] = getOneUDot(s, constrainedUDot, speedBody, speedIndex)
+               - function->calcDerivative(components, temp);
 }
 
 
 void Constraint::PrescribedVelocityImpl::
 addInVelocityConstraintForces
    (const State&                                s,
-	const Array_<Real>&                         multipliers,
-	Array_<SpatialVec, ConstrainedBodyIndex>&    bodyForces,
-	Array_<Real, ConstrainedUIndex>&             mobilityForces) const
+    const Array_<Real>&                         multipliers,
+    Array_<SpatialVec, ConstrainedBodyIndex>&   bodyForces,
+    Array_<Real, ConstrainedUIndex>&            mobilityForces) const
 {
-	const Real fq = multipliers[0];
-	addInOneMobilityForce(s, speedBody, speedIndex, fq, mobilityForces);
+    const Real fq = multipliers[0];
+    addInOneMobilityForce(s, speedBody, speedIndex, fq, mobilityForces);
 }
 
 
