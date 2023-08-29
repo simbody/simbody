72fcdc9383468e2f909fa15d24f2c1a9287d0e60
Clay Anderson <fcanderson@users.noreply.github.com>
2023-08-28 19:03:13 -0500

    Removed spaces in snprintf() calls
    This was done so that the snprintf() calls are consistent with the original sprintf() calls.

--- SimTKcommon/include/SimTKcommon/internal/String.h
+++ SimTKcommon/include/SimTKcommon/internal/String.h
@@ -118,22 +118,22 @@ Inf, -Inf). **/
 /*@{*/
 /** Format an int as a printable %String. **/
 explicit String(int i, const char* fmt="%d")
-{   const int n=32; char buf[n]; snprintf(buf, n, fmt,i); (*this)=buf; }
+{   const int n=32; char buf[n]; snprintf(buf,n,fmt,i); (*this)=buf; }
 /** Format a long as a printable %String. **/
 explicit String(long i, const char* fmt="%ld")
-{   const int n=64; char buf[n]; snprintf(buf, n, fmt,i); (*this)=buf; }
+{   const int n=64; char buf[n]; snprintf(buf,n,fmt,i); (*this)=buf; }
 /** Format a long long as a printable %String. **/
 explicit String(long long i, const char* fmt="%lld")
-{   const int n=64; char buf[n]; snprintf(buf, n, fmt,i); (*this)=buf; }
+{   const int n=64; char buf[n]; snprintf(buf,n,fmt,i); (*this)=buf; }
 /** Format an unsigned int as a printable %String. **/
 explicit String(unsigned int s, const char* fmt="%u")
-{   const int n=32; char buf[n]; snprintf(buf, n, fmt,s); (*this)=buf; }
+{   const int n=32; char buf[n]; snprintf(buf,n,fmt,s); (*this)=buf; }
 /** Format an unsigned long as a printable %String. **/
 explicit String(unsigned long s, const char* fmt="%lu")
-{   const int n=64; char buf[n]; snprintf(buf, n, fmt,s); (*this)=buf; }
+{   const int n=64; char buf[n]; snprintf(buf,n,fmt,s); (*this)=buf; }
 /** Format an unsigned long long as a printable %String. **/
 explicit String(unsigned long long s, const char* fmt="%llu")
-{   const int n=64; char buf[n]; snprintf(buf, n, fmt,s); (*this)=buf; }
+{   const int n=64; char buf[n]; snprintf(buf,n,fmt,s); (*this)=buf; }
 
 /** Format a float as a printable %String. Nonfinite values are formatted as
 NaN, Inf, or -Inf as appropriate (Matlab compatible). The default format
