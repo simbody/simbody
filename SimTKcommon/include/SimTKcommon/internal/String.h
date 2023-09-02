c40b9a49d703b75dff9b3da9c246a7f8dffb2a0c
Clay Anderson <fcanderson@users.noreply.github.com>
2023-09-02 15:39:50 -0500

    Precision set to LosslessNumDigitsReal in String::String(T)
    The templatized constructor SimTK::String::String(const T& value) can be used to convert a value of type T to a String provided class T has implemented operator<<().
    
    Previously, conversion was done using the default precision of std::ostringstream, which is typically ~6.  While an output precision of 6 is sufficient for many applications, it is insufficient when lossless conversion is needed, such as during serialization.
    
    To achieve lossless conversion, the ostringstream precision in SimTK::String::String(const T&) is now set to a value of SimTK::LosslessNumDigitsReal, which is typically 20.
    
    This change will affect SimTK::Xml::setValueAs<T>(const T& value). Now, XML elements representing doubles will have ~20 significant digits instead of ~6, rendering class SimTK::Xml a viable framework for lossless serialization.
    
    The Doxygen compliant comments for SimTK::String::String(const T& value) have been revised.

--- SimTKcommon/include/SimTKcommon/internal/String.h
+++ SimTKcommon/include/SimTKcommon/internal/String.h
@@ -41,6 +41,8 @@
 
 namespace SimTK {
 
+extern SimTK_SimTKCOMMON_EXPORT const int LosslessNumDigitsReal;
+
 template <class N> class negator;
 template <class R> class conjugate;
 
@@ -357,12 +359,15 @@ auto stringStreamExtractHelper(std::istringstream& is, T& t, int)
 
 /** Generic templatized %String constructor uses stream insertion
 `operator<<(T)` to generate the %String when no specialization of this
-constructor is available. A *runtime* error is thrown if this method is
+constructor is available. The generated String will have a sufficient number
+of significant digits (i.e., up to ~20) to represent each converted
+SimTK::Real without loss. A *runtime* error is thrown if this method is
 invoked and neither a specialization nor stream insertion operator is
 available. **/
 template <class T> inline
 String::String(const T& t) {
     std::ostringstream os;
+    os << std::setprecision(LosslessNumDigitsReal);
     *this = stringStreamInsertHelper(os, t, true).str();
 }
 
