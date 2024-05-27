8c9cb144a7a2dfd1dfc57e2574d519edb63fdc55
pepbos <pepijnvandenbos@gmail.com>
2024-05-27 14:33:53 +0200

    add license, author etc for CableSpan files

--- Simbody/src/CableSpan_SubSystem_Impl.h
+++ Simbody/src/CableSpan_SubSystem_Impl.h
@@ -1,6 +1,29 @@
 #ifndef SimTK_SIMBODY_CABLE_SPAN_SUBSYSTEM_IMPL_H_
 #define SimTK_SIMBODY_CABLE_SPAN_SUBSYSTEM_IMPL_H_
 
+/* -------------------------------------------------------------------------- *
+ *                           Simbody(tm)                                      *
+ * -------------------------------------------------------------------------- *
+ * This is part of the SimTK biosimulation toolkit originating from           *
+ * Simbios, the NIH National Center for Physics-Based Simulation of           *
+ * Biological Structures at Stanford, funded under the NIH Roadmap for        *
+ * Medical Research, grant U54 GM072970. See https://simtk.org/home/simbody.  *
+ *                                                                            *
+ * Portions copyright (c) 2012 Stanford University and the Authors.           *
+ * Authors: Pepijn van den Bos                                                *
+ * Contributors:                                                              *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain a  *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.         *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
+
 #include "simbody/internal/CableSpan.h"
 #include "simbody/internal/MultibodySystem.h"
 
